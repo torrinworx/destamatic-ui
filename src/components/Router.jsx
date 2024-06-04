9e0ab9e416be3e2d685e6b34a30c71292a7c4fc4
torrin1 <torrin@worx4you.com>
2024-06-04 11:50:47 -0400

    Adding shared object for future use

---
+++ src/components/Router.jsx
@@ -0,0 +1,10 @@
+import { h, OObject, Observer } from 'destam-dom';
+
+const Router = () => {
+    const currentRoute = Observer.mutable('');
+    const routes = OObject({});
+
+    return
+}
+
+export default Router;
