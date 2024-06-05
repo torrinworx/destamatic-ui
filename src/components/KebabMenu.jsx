4f907809a0abd1a6ba04169831cb1b96d53afbd8
Max <max@mapleprecision.com>
2024-06-05 12:08:06 -0400

    Fix up KebabMenu

--- src/components/KebabMenu.jsx
+++ src/components/KebabMenu.jsx
@@ -3,22 +3,25 @@ import h from './h';
 import Button from './Button';
 import Theme from './Theme';
 
-const KebabMenu = ({state}, _, mount) => {
-    // Close on reload
-    mount((e) => {
-        state._kebab = null
-    })
-  
+const KebabMenu = ({state}, cleanup) => {
+    
+    if (!state) {
+        state = Observer.mutable(false);
+    };
+
     const kebab = state.observer.path('_kebab')
 
-    // Close if not clicking kebab
-    window.addEventListener('click', e => {
+    const closeKebab = e => {
         if (!state._kebab) return;
         const coords = kebab.get().coords
         if (e.pageX !== coords[0] || e.pageY !== coords[1]){
             state._kebab = null
         }
-    });
+    }
+
+    // Close if not clicking kebab
+    window.addEventListener('click', closeKebab);
+    cleanup(() => window.removeEventListener('click', closeKebab))
 
     const Item = ({ each: item }) => {
         return <Button label={item.label} style={{ width: '100%' }} onClick={item.onClick}/>
@@ -26,8 +29,16 @@ const KebabMenu = ({state}, _, mount) => {
 
     // Create menu where user clicked
     return kebab.map(kebab => kebab ?
-    <div $style={{ position: 'absolute', left: `${kebab.coords[0]}px`, top: `${kebab.coords[1]}px`, zIndex: 2, 
-        border: `1px solid ${Theme.colours.secondary.base}`, backgroundColor: 'white', borderRadius: Theme.borderRadius, display: 'flex', flexDirection: 'column' }}>
+    <div $style={{
+        position: 'absolute',
+        left: `${kebab.coords[0]}px`,
+        top: `${kebab.coords[1]}px`,
+        zIndex: 2, 
+        border: `1px solid ${Theme.colours.secondary.base}`,
+        backgroundColor: 'white',
+        borderRadius: Theme.borderRadius, display: 'flex',
+        flexDirection: 'column'
+    }}>
         <Item each={kebab.items} />
     </div> : null
     )
