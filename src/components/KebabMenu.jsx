422b4af16164d4af9f78417e86de496883f0ef03
Max <max@mapleprecision.com>
2024-06-06 11:16:14 -0400

    Mount Click Handler only when opened

--- src/components/KebabMenu.jsx
+++ src/components/KebabMenu.jsx
@@ -5,33 +5,45 @@ import Button from './Button';
 import Shown from './Shown';
 import Theme from './Theme';
 
-const KebabMenu = ({value, items}, cleanup) => {
+const KebabMenu = ({value, items, closeOnClick = true}, cleanup, mount) => {
 
     if (!value) {
         value = Observer.mutable(false);
     };
 
-    const closeKebab = e => {
-        const coords = value.get();
-        if (e.pageX !== coords?.[0] || e.pageY !== coords?.[1]){
-            value.set();
-        }
-    }
+    const Div = <div />;
 
     // Close if not clicking kebab
-    window.addEventListener('click', closeKebab);
-    cleanup(() => window.removeEventListener('click', closeKebab));
+    const close = e => {
+        if (!value.get() || e.pageX === value.get().x || e.pageY === value.get().y) return
+        let current = e.target;
+        while (current && current !==  Div){
+            current = current.parentElement;
+        }
+        if (!current) value.set(false);
+    }
 
     const Item = ({ each: item }) => {
-        return <Button label={item.label} style={{ width: '100%' }} onClick={item.onClick}/>
+        return <Button label={item.label} style={{ width: '100%' }} onClick={() => {
+            if (closeOnClick) value.set(false)
+            item.onClick()
+        }}/>
+    }
+
+    // Listen for click events only when mounted
+    const ClickHandler = ({}, cleanup, mount) => {
+        mount(window.addEventListener('click', close));
+        cleanup(() => window.removeEventListener('click', close));
+
+        return null;
     }
 
     // Create menu where user clicked
     return <Shown value={value}>
-        <div $style={{
+        <Div $style={{
             position: 'absolute',
-            left: value.map(value => value?.[0] + 'px'),
-            top: value.map(value => value?.[1] + 'px'),
+            left: value.map(value => value?.x + 'px'),
+            top: value.map(value => value?.y + 'px'),
             zIndex: 2, 
             border: `1px solid ${Theme.colours.secondary.base}`,
             backgroundColor: 'white',
@@ -39,8 +51,9 @@ const KebabMenu = ({value, items}, cleanup) => {
             display: 'flex',
             flexDirection: 'column'
         }}>
+            <ClickHandler />
             <Item each={items} />
-        </div>
+        </Div>
     </Shown>
 }
 
