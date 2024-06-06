0bcb004358055eaa9d5819d049e265d5af733f89
Max <max@mapleprecision.com>
2024-06-06 09:56:04 -0400

    Standardize KebabMenu to other components

--- src/components/KebabMenu.jsx
+++ src/components/KebabMenu.jsx
@@ -1,49 +1,46 @@
 import h from './h';
+import { Observer } from 'destam-dom';
 
 import Button from './Button';
 import Shown from './Shown';
 import Theme from './Theme';
 
-const KebabMenu = ({state}, cleanup) => {
-    
-    if (!state) {
-        state = Observer.mutable(false);
+const KebabMenu = ({value, items}, cleanup) => {
+
+    if (!value) {
+        value = Observer.mutable(false);
     };
 
-    const kebab = state.observer.path('_kebab')
-
     const closeKebab = e => {
-        if (!state._kebab) return;
-        const coords = kebab.get().coords
-        if (e.pageX !== coords[0] || e.pageY !== coords[1]){
-            state._kebab = null
+        const coords = value.get();
+        if (e.pageX !== coords?.[0] || e.pageY !== coords?.[1]){
+            value.set();
         }
     }
 
     // Close if not clicking kebab
     window.addEventListener('click', closeKebab);
-    cleanup(() => window.removeEventListener('click', closeKebab))
+    cleanup(() => window.removeEventListener('click', closeKebab));
 
     const Item = ({ each: item }) => {
         return <Button label={item.label} style={{ width: '100%' }} onClick={item.onClick}/>
     }
 
     // Create menu where user clicked
-    return <Shown value={kebab}>
-        {kebab.map(kebab => {
-            return <div $style={{
-                position: 'absolute',
-                left: `${kebab?.coords[0]}px`,
-                top: `${kebab?.coords[1]}px`,
-                zIndex: 2, 
-                border: `1px solid ${Theme.colours.secondary.base}`,
-                backgroundColor: 'white',
-                borderRadius: Theme.borderRadius, display: 'flex',
-                flexDirection: 'column'
-            }}>
-                <Item each={kebab?.items} />
-            </div>
-        })}
+    return <Shown value={value}>
+        <div $style={{
+            position: 'absolute',
+            left: value.map(value => value?.[0] + 'px'),
+            top: value.map(value => value?.[1] + 'px'),
+            zIndex: 2, 
+            border: `1px solid ${Theme.colours.secondary.base}`,
+            backgroundColor: 'white',
+            borderRadius: Theme.borderRadius,
+            display: 'flex',
+            flexDirection: 'column'
+        }}>
+            <Item each={items} />
+        </div>
     </Shown>
 }
 
