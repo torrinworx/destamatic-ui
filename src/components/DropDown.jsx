cc651756ca801bfdae5bf8fda213746623ca8e41
Max <max@mapleprecision.com>
2024-06-12 09:57:11 -0400

    DropDown arrow position

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -4,13 +4,14 @@ import Shown from './Shown';
 import { Observer } from 'destam-dom';
 import Icon from './Icon';
 
-const DropDown = ({ children, label }) => {
+const DropDown = ({ children, label, arrow = 'right' }) => {
     const isDropedDown = Observer.mutable(false)
 
     const toggle = () => isDropedDown.set(!isDropedDown.get())
 
     return <div>
-        <div $style={{ display: 'flex', alignItems: 'center' }} $onclick={toggle}>
+        <div $style={{ display: 'flex', alignItems: 'center', justifyContent: arrow === 'right' ? 'space-between' : 'flex-start' }} $onclick={toggle}>
+            {arrow === 'right' ? label : null}
             <div $style={{ display: 'flex', alignItems: 'center' }}>
                 <i $class='chevron-icon' $style={{ cursor: 'pointer' }}>
                     {isDropedDown.map((show) => show ?
@@ -18,7 +19,7 @@ const DropDown = ({ children, label }) => {
                         <Icon size='20' libraryName='feather' iconName='chevron-right' />)}
                 </i>
             </div>
-            {label}
+            {arrow === 'left' ? label : null}
         </div>
         <Shown value={isDropedDown} children={children} />
     </div>
