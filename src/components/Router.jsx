fbb7cca36c0ef00e167fd17877cc0c927812da6b
Alexander Orzechowski <alex@ozal.ski>
2024-07-02 10:15:26 -0400

    Router: Don't do anything special with 'Shared'
    Just pass all the rest of the props like normal

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -8,20 +8,19 @@ export const currentRoute = Observer.mutable('');
 
 /**
  * Router component that dynamically renders components based on the current route.
- * 
+ *
  * @param {Object} routes - An object where keys represent route names and values represent corresponding components.
- * @param {Object} Shared - A global context observer for shared data and state across components.
  * @param {Function} [NotFound] - Optional. A component to render when the route is not found.
  * @returns {HTMLElement} The component that matches the current route, or a 404 message if no match is found.
  */
-export const Router = ({ routes, Shared, NotFound, ...props }) => {
+export const Router = ({ routes, NotFound, ...props }) => {
     return currentRoute.map(route => {
         // Default to the root route if no path is provided
         if (!route) route = '/';
 
         const Component = routes[route];
-        return Component 
-            ? <Component Shared={Shared} {...props} /> 
-            : (NotFound ? <NotFound Shared={Shared} {...props} /> : <div>404 - Not Found</div>);
+        return Component
+            ? <Component {...props} />
+            : (NotFound ? <NotFound {...props} /> : <div>404 - Not Found</div>);
     });
 };
