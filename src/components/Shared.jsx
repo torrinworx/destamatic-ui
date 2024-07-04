b02376a846b72e7059352eee45173d1126e9f34a
torrin1 <torrin@worx4you.com>
2024-07-04 11:22:11 -0400

    removing comment

--- src/components/Shared.jsx
+++ src/components/Shared.jsx
@@ -13,25 +13,4 @@ const Shared = OObject({
     currentRoute: currentRoute,
 });
 
-/*
-Ideally we should have some kind of system like this that automatically injects
-Shared variable into all child components so that whenever a user imports a 
-destamatic-ui component, all child componentns have Shared.
-
-Something like this:
-window.addEventListener('load', () => {
-	remove = mount(document.body, <SharedProvider Shared={Shared}>
-            <Router routes={routes} />
-        </SharedProvider>
-    );
-});
-
-But I haven't figured out a way to do that recursively so that all child
-components have access to Shared from the mount() function.
-
-Currently when we import it into a new project and import Shared the
-declaration get's re-instantiated when we import it into all components in
-destamatic-ui.
-*/
-
 export default Shared
