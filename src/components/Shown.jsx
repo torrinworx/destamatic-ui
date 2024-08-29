fd5eb3566be1c6bb52aa290d633d19325deefe1b
Alexander Orzechowski <alex@ozal.ski>
2024-08-29 10:51:52 -0400

    Add documentation about Shown and marks

--- src/components/Shown.jsx
+++ src/components/Shown.jsx
@@ -1,6 +1,20 @@
 import {mark} from './h';
 import Observer from 'destam/Observer';
 
+/**
+ * Conditionally shows elements depending on the value.
+ *
+ * This component supports marks. Marks are special elements that are not
+ * meant to be renderered, but instead hold metadata that the user might want
+ * to specify. This metadata can be read by the Shown component to implement
+ * special behavior. Two marks are supported:
+ *
+ * <mark:then> Marks any children components as being shown when the value is true.
+ * <mark:else> Marks any children components as being shown when whe value is false.
+ *
+ * By default, if a component is not marked, it will be assumed that it will be shown
+ * when the value is true.
+ */
 const Shown = ({children, invert = false, value}) => {
 	const [truthy, falsy] = children.reduce((a, c) => {
 		if (c instanceof mark) {
