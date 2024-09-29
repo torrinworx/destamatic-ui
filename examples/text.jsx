557fc212f769326246a494ca156ea0f9504a3a75
Alexander Orzechowski <alex@ozal.ski>
2024-09-28 21:57:03 -0400

    Add test example

---
+++ examples/text.jsx
@@ -0,0 +1,10 @@
+import { Observer, mount } from 'destam-dom';
+import TextField from 'destamatic-ui/TextField';
+
+const input = Observer.mutable('');
+
+mount(document.body, <>
+	<TextField value={input} />
+	<TextField value={input} />
+	<TextField value={Observer.immutable("you cannot change this text")} />
+</>);
