624a7eb23b31dfbe3a016441ec7fb4ba141fda39
Alexander Orzechowski <alex@ozal.ski>
2024-09-30 00:26:03 -0400

    Paper: add shadow

--- src/Paper.jsx
+++ src/Paper.jsx
@@ -3,8 +3,10 @@ import { h } from './h.jsx';
 const Paper = ({children, style}) => {
 	return <div style={{
 		borderRadius: '5px',
-		background: 'white',
-		margin: '10px',
+		background: '#EEE',
+	    boxShadow: '4px 4px 10px rgba(0,0,0,0.2)',
+	    insetBoxShadow: 'inset -2px -2px 10px rgba(0,0,0,0.2)',
+		padding: '10px',
 		...style
 	}}>
 		{children}
