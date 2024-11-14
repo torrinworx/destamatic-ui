48cd1b58c4e1d31dda7923650897a5163ceb42ba
torrin1 <torrin@worx4you.com>
2024-11-14 12:35:55 -0500

    Updating checkbox

--- components/Checkbox.jsx
+++ components/Checkbox.jsx
@@ -5,49 +5,60 @@ import Theme from './Theme';
 
 Theme.define({
 	checkbox: {
-		extends: 'primary',
+		extends: 'primary_focusable',
 		accentColor: '$color',
+		height: '20px',
+		width: '20px'
+	},
+	checkboxwrapper: {
+		padding: "8px",
+		overflow: 'clip',
+		borderRadius: '50%',
+		position: 'relative',
+		display: 'inline-block',
+		transition: 'background 250ms',
+	},
+	checkboxwrapper_hovered: {
+		extends: 'primary',
+		background: '$color_hover'
 	},
 });
 
 /**
  * Checkbox component.
  *
- * This component renders a checkbox input that is controlled via an observable state (OValue).
- * It also provides a callback function (onChange) to handle state changes and can receive custom styles.
+ * This component renders a checkbox input that is managed through an observable value.
+ * It supports a callback function (onChange) to react to state changes and allows for custom styling.
+ * In addition, it can be inverted and disabled based on the specified flags.
  *
  * @param {Object} props - The properties object.
- * @param {Observer<boolean>|boolean} [props.value]] - Observable selected state or a boolean value.
- * @param {function} [props.onChange] - Callback function to call when the selected state changes.
- * @param {Object} [props.style] - Custom styles to apply to the component.
- * @param {Object} [props.rest] - Additional props to pass to the input element.
- * 
+ * @param {Observer<boolean>|boolean} [props.value] - Observable representing the selected state or a static boolean value.
+ * @param {function} [props.onChange] - Function to be executed when the selected state changes.
+ * @param {boolean} [props.invert=false] - Flag to invert the appearance or behavior of the checkbox.
+ * @param {Observer<boolean>|boolean} [props.disabled] - Observable representing the disabled state or a static boolean value.
+ * @param {Object} [props.style] - Custom styles to apply directly to the checkbox input.
+ * @param {Object} [props.rest] - Additional props to be propagated to the input element.
+ *
  * @returns {JSX.Element} The rendered Checkbox component.
  */
 const Checkbox = ({ value, onChange, invert = false, disabled, style, ...props }) => {
-    if (!(value instanceof Observer)) value = Observer.immutable(value);
-    if (!(disabled instanceof Observer)) disabled = Observer.immutable(disabled);
+	if (!(value instanceof Observer)) value = Observer.immutable(value);
+	if (!(disabled instanceof Observer)) disabled = Observer.immutable(disabled);
 
 	const [ripples, createRipple] = useRipples();
 	const hover = Observer.mutable(false);
 
-	const Span = <span />;
-	const Input = <input />;
+	const Span = <raw:span />;
+	const Input = <raw:input />;
 
-    return <Span
-		style={{
-			padding: "8px",
-			overflow: 'clip',
-			position: 'relative',
-			borderRadius: '50%',
-			background: hover.map(h => h ? 'rgba(0, 0, 0, .1)' : null),
-			transition: 'background 250ms',
-			display: 'inline-block',
-			...style
-		}}
-		onMouseEnter={() => hover.set(true)}
-		onMouseLeave={() => hover.set(false)}
-		onClick={e => {
+	return <Span
+		theme={[
+			'checkboxwrapper',
+			disabled.map(d => d ? 'disabled' : null),
+			hover.map(h => h ? 'hovered' : null),
+		]}
+		isHovered={hover}
+		onMouseDown={e => {
 			if (disabled.get()) {
 				return;
 			}
@@ -74,9 +85,10 @@ const Checkbox = ({ value, onChange, invert = false, disabled, style, ...props }
 				"checkbox",
 				disabled.map(d => d ? 'disabled' : null)
 			]}
-			$checked={value.map(v => v !== invert)}
+			$checked={value}
 			{...props}
 		/>
+		<span theme={[disabled.map(d => d ? 'disabledoverlay' : null)]} />
 		{ripples}
 	</Span>
 };
