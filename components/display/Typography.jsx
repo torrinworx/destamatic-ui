5e52d1717cf004578c3f26ac5290d7f44f5f217f
Alexander Orzechowski <alex@ozal.ski>
2024-12-19 13:14:38 -0500

    Typography: set textwrap for h

--- components/display/Typography.jsx
+++ components/display/Typography.jsx
@@ -2,18 +2,26 @@ import Theme from '../utils/Theme';
 import ThemeContext from '../utils/ThemeContext';
 
 Theme.define({
-	typography_h1: { fontSize: 62 },
-	typography_h2: { fontSize: 56 },
-	typography_h3: { fontSize: 36 },
-	typography_h4: { fontSize: 30 },
-	typography_h5: { fontSize: 24 },
-	typography_h6: { fontSize: 20 },
+	typography: {
+		display: 'flex',
+	},
+
+	typography_h1: { fontSize: 62, textWrap: 'nowrap' },
+	typography_h2: { fontSize: 56, textWrap: 'nowrap' },
+	typography_h3: { fontSize: 36, textWrap: 'nowrap' },
+	typography_h4: { fontSize: 30, textWrap: 'nowrap' },
+	typography_h5: { fontSize: 24, textWrap: 'nowrap' },
+	typography_h6: { fontSize: 20, textWrap: 'nowrap' },
 	typography_p1: { fontSize: 16 },
 	typography_p2: { fontSize: 14 },
 	typography_regular: { fontStyle: 'normal' },
 	typography_bold: { fontWeight: 'bold' },
 	typography_italic: { fontStyle: 'italic' },
 	typography_center: { textAlign: 'center' },
+
+	typography_inline: {
+		display: 'inline-flex',
+	}
 });
 
 export default ThemeContext.use(h => {
@@ -29,17 +37,12 @@ export default ThemeContext.use(h => {
 	 *
 	 * @returns {JSX.Element} The rendered typography element.
 	 */
-	const Typography = ({ inline, type = 'h1', fontStyle = 'regular', bold, center, children, style, ...props }) => {
+	const Typography = ({ inline, type = 'h1', fontStyle = 'regular', bold, center, children, ...props }) => {
 		if (bold) fontStyle = 'bold';
 
 		return <div
-			style={{
-
-				display: inline ? 'inline-flex' : 'flex',
-				...style
-			}}
 			{...props}
-			theme={['typography', type, center ? 'center' : null, fontStyle]}
+			theme={['typography', type, center ? 'center' : null, inline ? 'inline' : null, fontStyle]}
 		>
 			{children}
 		</div>;
