04ea19b1bf131d5e90ba5dc34398364bbc46d791
Alexander Orzechowski <alex@ozal.ski>
2024-12-23 18:45:58 -0500

    Detached/Select: Redo with a more generalized API
    Detached will no longer force that the anchor content is a button

--- components/inputs/Select.jsx
+++ components/inputs/Select.jsx
@@ -1,4 +1,4 @@
-import { h } from '../utils/h';
+import { h, mark } from '../utils/h';
 import Observer from 'destam/Observer';
 import Typography from '../display/Typography';
 import Icon from '../display/Icon';
@@ -6,27 +6,30 @@ import Paper from '../display/Paper';
 import useRipples from '../utils/Ripple';
 import Theme from '../utils/Theme';
 import Detached from '../utils/Detached';
+import Button from '../inputs/Button';
 
 Theme.define({
 	select_selectable: {
 		width: '100%',
 		boxSizing: 'border-box',
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-		userSelect: 'none',
-		position: 'relative',
 		overflow: 'clip',
-		padding: 8,
-		cursor: 'pointer',
+		borderRadius: 0,
 	},
 
 	select_selected: {
-		background: '#CCCCCC'
+		background: '$shiftBrightness($invert($color_top), 0.2)',
+	},
+
+	select_selected_hovered: {
+		background: '$shiftBrightness($invert($color_top), 0.2)',
+	},
+
+	select_hovered: {
+		background: '$shiftBrightness($invert($color_top), 0.1)',
 	},
 });
 
-export const Select = ({ value, options, display, style, menu }) => {
+const Select = ({ value, options, display, style }) => {
 	if (!(value instanceof Observer)) value = Observer.immutable(value);
 	if (!(options instanceof Observer)) options = Observer.immutable(options);
 	if (!display) display = a => a;
@@ -34,43 +37,55 @@ export const Select = ({ value, options, display, style, menu }) => {
 	const focused = Observer.mutable(false);
 	const selector = value.selector('selected', null);
 
-	const Selectable = ({ each: option }) => {
-		const [ripples, createRipple] = useRipples();
+	const buttonRef = <raw:button />;
 
-		return <div
-			theme={[
-				"select_selectable",
-				selector(option)
-			]}
-			onMouseDown={e => {
-				createRipple(e);
-				value.set(option);
-				focused.set(false);
-			}}
-		>
-			{display(option)}
-			{ripples}
-		</div>;
-	};
+	const Popup = ({}, cleanup, mounted) => {
+		const Selectable = ({ each: option }) => {
+			return <Button
+				type={[
+					"select_selectable",
+					selector(option)
+				]}
+				onMouseUp={e => {
+					value.set(option);
+					focused.set(false);
+				}}
+			>
+				{display(option)}
+			</Button>;
+		};
 
-	return <Detached style={style} enabled={focused} menu={
-		menu ? menu :
-			<>
-				<Typography type='p1' style={{ display: 'inline' }}>
-					{value.map(val => {
-						if (options.get().includes(val)) {
-							return display(val);
-						} else {
-							return "None";
-						}
-					})}
-				</Typography>
-				<Icon lib="feather" name="play" size={16} style={{ marginLeft: 10, transform: 'rotate(90deg)' }} />
-			</>
-	}>
-		<Paper style={{ minWidth: 100, padding: 0, overflow: 'auto' }}>
+		return <Paper tight style={{ minWidth: buttonRef.clientWidth, overflow: 'auto' }}>
 			<Selectable each={options} />
 		</Paper>
+	};
+
+	return <Detached enabled={focused}>
+		<Button ref={buttonRef} style={style} onMouseDown={e => focused.set(true)}>
+			<Typography type='p1' inline>
+				{value.map(val => {
+					if (options.get().includes(val)) {
+						return display(val);
+					} else {
+						return "None";
+					}
+				})}
+			</Typography>
+			<Icon
+				lib="feather"
+				name="chevron-down"
+				size={16}
+				style={{
+					marginLeft: 10,
+					transform: focused.map(f => f ? 'rotate(180deg)' : null),
+					transition: 'transform 100ms ease-in-out',
+				}}
+			/>
+		</Button>
+
+		<mark:popup>
+			<Popup />
+		</mark:popup>
 	</Detached>;
 };
 
--- components/utils/Detached.jsx
+++ components/utils/Detached.jsx
@@ -1,8 +1,10 @@
-import { h } from './h';
+import { mark } from './h';
 import Observer from 'destam/Observer';
 import Popup from './Popup';
 import Shown from './Shown';
-import Button from '../inputs/Button';
+import ThemeContext from './ThemeContext';
+
+import { mount } from 'destam-dom';
 
 const clamp = (x, l, h) => Math.max(l, Math.min(h, x));
 
@@ -37,87 +39,166 @@ const calculate = (bounds, rot) => {
 	};
 };
 
-const Detached = ({ menu, type = 'text', children, enabled, style, icon, focusable = true }) => {
-	const focused = enabled || Observer.mutable(false);
+const getBounds = (elems) => {
+	let left = Infinity, right = -Infinity, top = Infinity, bottom = -Infinity;
 
-	const A = <raw:button />;
-	const popup = <raw:div />;
+	for (const elem of elems) {
+		const bounds = elem.getBoundingClientRect();
 
-	const computed = Observer.mutable(false);
+		left = Math.min(left, bounds.left);
+		right = Math.max(right, bounds.right);
+		top = Math.min(top, bounds.top);
+		bottom = Math.max(bottom, bounds.bottom);
+	}
 
-	const Contents = ({}, cleanup, mounted) => {
-		mounted(() => {
-			computed.set(true);
-		});
+	return {
+		left, right, top, bottom,
+		width: right - left,
+		height: bottom - top,
+	};
+};
 
-		return children;
+const trackedMount = (children) => {
+	const elems = [];
+	const out = (elem, val, before, context) => {
+		const mountedElem = {
+			insertBefore: (node, before) => {
+				elems.push(node);
+				elem.insertBefore(node, before);
+			},
+			replaceChild: (newNode, oldNode) => {
+				const i = elems.indexOf(oldNode);
+				elems[i] = newNode;
+
+				elem.replaceChild(newNode, oldNode);
+			},
+			removeChild: (node) => {
+				const i = elems.indexOf(node);
+				elems.splice(i, 1);
+
+				elem.removeChild(node);
+			},
+		};
+
+		return mount(mountedElem, children, before, context);
 	};
 
-	return <>
-		<Button
-			type={[
-				type,
-				...(focusable ? ['focusable', focused.map(f => f ? 'focused' : null)] : [])
-			]}
-			Icon={icon}
-			style={style}
-			ref={A}
-			onClick={() => {
-				if (typeof focused.get() === 'number') {
-					focused.set(false);
-					return;
-				}
-
-				computed.set(false);
-				focused.set(true);
-				const bounds = popup.getBoundingClientRect();
-				const surround = A.getBoundingClientRect();
-
-				const ww = window.innerWidth;
-				const wh = window.innerHeight;
-
-				let fits = [];
-				for (const rot of [6, 5, 1, 2, 0, 7, 3, 4]) {
-					const calc = calculate(surround, rot);
-
-					if ('right' in calc) calc.left = ww - calc.right - Math.min(bounds.width, calc.maxWidth);
-					if ('left' in calc) calc.right = ww - calc.left - Math.min(bounds.width, calc.maxWidth);
-					if ('bottom' in calc) calc.top = wh - calc.bottom - Math.min(bounds.height, calc.maxHeight);
-					if ('top' in calc) calc.bottom = wh - calc.top - Math.min(bounds.height, calc.maxHeight);
-
-					const width = clamp(ww - calc.right, 0, ww) - clamp(calc.left, 0, ww);
-					const height = clamp(wh - calc.bottom, 0, wh) - clamp(calc.top, 0, wh);
-					fits.push({ size: width * height, rot });
-				}
-
-				fits.sort((a, b) => b.size - a.size);
-
-				if (!fits.length) {
-					focused.set(false);
-				} else {
-					focused.set(fits[0].rot);
-				}
-			}}
-		>
-			{menu}
-		</Button>
-		<Shown value={focused.map(v => typeof v === 'number' || v === true)}>
-			<Popup
-				ref={popup}
-				placement={focused.map(rot => {
-					if (typeof rot !== 'number') return null;
-
-					const bounds = A.getBoundingClientRect();
-					return calculate(bounds, rot);
-				}).setter(() => focused.set(false))}
-				style={{
-					visibility: computed.map(c => c ? 'visible' : 'hidden'),
-				}}
-			>
-				<Contents />
-			</Popup>
-		</Shown>
-	</>;
+	out.elems = elems;
+	return out;
 };
+
+const Detached = ThemeContext.use(h => {
+	const Detached = ({ children, locations = defaultLocations, enabled }, cleanup) => {
+		const focused = enabled || Observer.mutable(false);
+
+		const popupRef = <raw:div />;
+
+		const computed = Observer.mutable(false);
+
+		const [focusedRender, focusedAfter] = focused.memo(2);
+		cleanup(focusedAfter.effect(value => {
+			if (value !== true) return;
+
+			const bounds = popupRef.getBoundingClientRect();
+			const surround = getBounds(elems.elems);
+
+			const ww = window.innerWidth;
+			const wh = window.innerHeight;
+
+			let fits = [];
+			for (const rot of locations) {
+				const calc = calculate(surround, rot);
+
+				if ('right' in calc) calc.left = ww - calc.right - Math.min(bounds.width, calc.maxWidth);
+				if ('left' in calc) calc.right = ww - calc.left - Math.min(bounds.width, calc.maxWidth);
+				if ('bottom' in calc) calc.top = wh - calc.bottom - Math.min(bounds.height, calc.maxHeight);
+				if ('top' in calc) calc.bottom = wh - calc.top - Math.min(bounds.height, calc.maxHeight);
+
+				const width = clamp(ww - calc.right, 0, ww) - clamp(calc.left, 0, ww);
+				const height = clamp(wh - calc.bottom, 0, wh) - clamp(calc.top, 0, wh);
+				fits.push({ size: width * height, rot });
+			}
+
+			fits.sort((a, b) => b.size - a.size);
+
+			if (!fits.length) {
+				focused.set(false);
+			} else {
+				focused.set(fits[0].rot);
+			}
+		}));
+
+		const popup = [];
+		const anchor = [];
+		for (const child of children) {
+			if (child instanceof mark) {
+				if (child.name === 'popup') {
+					popup.push(...child.props.children);
+				} else if (child.name === 'anchor') {
+					anchor.push(...child.props.children);
+				} else {
+					throw new Error("Detached only accepts popup and anchor mark elements");
+				}
+			} else {
+				anchor.push(child);
+			}
+		}
+
+		const Contents = ({}, cleanup, mounted) => {
+			mounted(() => {
+				computed.set(true);
+			});
+
+			cleanup(() => {
+				computed.set(false);
+			});
+
+			return popup;
+		};
+
+		const elems = trackedMount(anchor);
+		return <>
+			{elems}
+			<Shown value={focusedRender.map(v => typeof v === 'number' || v === true)}>
+				<Popup
+					ref={popupRef}
+					placement={focusedAfter.map(rot => {
+						if (typeof rot !== 'number') return null;
+
+						const bounds = getBounds(elems.elems);
+						return calculate(bounds, rot);
+					}).setter(() => focused.set(false))}
+					style={{
+						visibility: computed.map(c => c ? 'visible' : 'hidden'),
+					}}
+				>
+					<Contents />
+				</Popup>
+			</Shown>
+		</>;
+	};
+
+	return Detached;
+});
+
+Detached.TOP_LEFT_DOWN = 0;
+Detached.TOP_LEFT_RIGHT = 1;
+Detached.TOP_RIGHT_LEFT = 2;
+Detached.TOP_RIGHT_DOWN = 3;
+Detached.BOTTOM_RIGHT_UP = 4;
+Detached.BOTTOM_RIGHT_LEFT = 5;
+Detached.BOTTOM_LEFT_RIGHT = 6;
+Detached.BOTTOM_LEFT_UP = 7;
+
+const defaultLocations = [
+	Detached.BOTTOM_LEFT_RIGHT,
+	Detached.BOTTOM_RIGHT_LEFT,
+	Detached.TOP_LEFT_RIGHT,
+	Detached.TOP_RIGHT_LEFT,
+	Detached.TOP_LEFT_DOWN,
+	Detached.BOTTOM_LEFT_UP,
+	Detached.TOP_RIGHT_DOWN,
+	Detached.BOTTOM_RIGHT_UP,
+];
 
 export default Detached;
