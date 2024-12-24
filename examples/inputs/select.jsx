d4c5bad773ef6ba0e9310a2de539262cc4d32cc7
Alexander Orzechowski <alex@ozal.ski>
2024-12-23 22:18:35 -0500

    examples/select: Don't use kebab menu

--- examples/inputs/select.jsx
+++ examples/inputs/select.jsx
@@ -1,12 +1,15 @@
 import { Observer, mount } from 'destam-dom';
-import { Select, popups, Typography, KebabMenu, Paper } from 'destamatic-ui';
+import { Select, popups, Typography, Paper } from 'destamatic-ui';
 
 const input = Observer.mutable('');
 
 mount(document.body, <>
-	<Typography style={{display: 'flex', alignItems: 'center', justifyContent: 'center', inset: 0, position: 'absolute'}}>
-		{input}
-	</Typography>
+	<div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', inset: 0, position: 'absolute', flexDirection: 'column'}}>
+		<Typography>
+			{input}
+		</Typography>
+		<Select options={['one', 'two', 'three']} value={Observer.mutable('one')} />
+	</div>
 	{[
 		['left', 'top'],
 		['right', 'top'],
@@ -16,9 +19,5 @@ mount(document.body, <>
 		return <Select style={{position: 'absolute', [a]: 0, [b]: 0}} value={input} options={Array(100).fill(null).map((_, i) => i)} />
 	})}
 
-	<KebabMenu style={{position: 'absolute', left: 100, top: 100}} items={[
-		{label: 'Rename'},
-		{label: 'Remove'},
-	]} />
 	{popups}
 </>);
