499cdf0f1ca2ababcc95273db69f2fa43da2008a
torrin1 <torrin@worx4you.com>
2025-01-09 10:06:04 -0500

    organizing exports

--- index.jsx
+++ index.jsx
@@ -1,38 +1,44 @@
-export * from './components/utils/h.jsx';
-
-export { default as Button } from './components/inputs/Button.jsx';
-export { default as Drawer } from './components/navigation/Drawer.jsx';
-export { default as DropDown } from './components/navigation/DropDown.jsx';
-export { default as Icon } from './components/display/Icon.jsx';
-export { default as LoadingDots } from './components/utils/LoadingDots.jsx';
-export { default as Ripple } from './components/utils/Ripple.jsx';
-export { default as Shown } from './components/utils/Shown.jsx'
-export { default as TextArea } from './components/inputs/TextArea.jsx';
-export { default as Theme } from './components/utils/Theme.jsx';
-export { default as ThemeContext } from './components/utils/ThemeContext.jsx';
-export { default as Typography } from './components/display/Typography.jsx';
-export { default as Router } from './components/utils/Router.jsx';
-export { default as Radio } from './components/inputs/Radio.jsx';
-export { default as Checkbox } from './components/inputs/Checkbox.jsx';
-export { default as Tooltip } from './components/display/Tooltip.jsx';
-export { default as Markdown } from './components/display/Markdown.jsx';
-export { default as Link } from './components/display/Link.jsx';
+// ** display ** //
 // export { default as CodeBlock } from './components/CodeBlock.jsx';
-export { default as Slider } from './components/inputs/Slider.jsx';
-export { default as createContext } from './components/utils/Context.jsx';
-export { default as Paper } from './components/display/Paper.jsx';
-export { default as Tabs } from './components/navigation/Tabs.jsx';
-export { default as TextField } from './components/inputs/TextField.jsx';
-export { default as Select } from './components/inputs/Select.jsx';
-export { default as useRipples } from './components/utils/Ripple.jsx';
-export { default as Popup } from './components/utils/Popup.jsx';
-export { popups } from './components/utils/Popup.jsx';
-export { default as Detached } from './components/utils/Detached.jsx';
+export { default as Column } from './components/display/Column.jsx';
 export { default as Drag } from './components/display/Drag.jsx';
-export { default as Switch } from './components/inputs/Switch.jsx';
+export { default as Icon } from './components/display/Icon.jsx';
+export { default as Link } from './components/display/Link.jsx';
+export { default as Markdown } from './components/display/Markdown.jsx';
+export { default as Paper } from './components/display/Paper.jsx';
+export { default as Row } from './components/display/Row.jsx';
+export { default as Tooltip } from './components/display/Tooltip.jsx';
+export { default as Typography } from './components/display/Typography.jsx';
+
+// ** inputs ** //
+export { default as Button } from './components/inputs/Button.jsx';
+export { default as Checkbox } from './components/inputs/Checkbox.jsx';
 export { default as ColorPicker } from './components/inputs/ColorPicker.jsx';
 export { default as Date } from './components/inputs/Date.jsx';
+export { default as Radio } from './components/inputs/Radio.jsx';
+export { default as Select } from './components/inputs/Select.jsx';
+export { default as Slider } from './components/inputs/Slider.jsx';
+export { default as Switch } from './components/inputs/Switch.jsx';
+export { default as TextArea } from './components/inputs/TextArea.jsx';
+export { default as TextField } from './components/inputs/TextField.jsx';
+
+// ** navigation ** //
+export { default as Drawer } from './components/navigation/Drawer.jsx';
+export { default as DropDown } from './components/navigation/DropDown.jsx';
 export { default as Scroll } from './components/navigation/Scroll.jsx';
-export { default as Column } from './components/display/Column.jsx';
-export { default as Row } from './components/display/Row.jsx';
+export { default as Tabs } from './components/navigation/Tabs.jsx';
+
+// ** utils ** //
+export { default as createContext } from './components/utils/Context.jsx';
+export { default as Detached } from './components/utils/Detached.jsx';
+export * from './components/utils/h.jsx';
+export { default as LoadingDots } from './components/utils/LoadingDots.jsx';
+export { default as Popup } from './components/utils/Popup.jsx';
+export { popups } from './components/utils/Popup.jsx';
+export { default as Ripple } from './components/utils/Ripple.jsx';
+export { default as useRipples } from './components/utils/Ripple.jsx';
+export { default as Router } from './components/utils/Router.jsx';
+export { default as Shown } from './components/utils/Shown.jsx'
 export { default as suspend } from './components/utils/Suspend.jsx';
+export { default as Theme } from './components/utils/Theme.jsx';
+export { default as ThemeContext } from './components/utils/ThemeContext.jsx';
