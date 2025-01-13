84f044b169c87f4d7caebe1d9838f8eb5a585517
Alexander Orzechowski <alex@ozal.ski>
2025-01-12 19:34:47 -0500

    Divider: Reformat

--- components/display/Divider.jsx
+++ components/display/Divider.jsx
@@ -4,66 +4,69 @@ import ThemeContext from '../utils/ThemeContext';
 
 Theme.define({
 	divider: {
-        width: 4,
-        cursor: 'ew-resize',
-        background: '#E5E7EB'
+		width: 4,
+		cursor: 'ew-resize',
+		background: '#E5E7EB'
 	}
 });
 
 export default ThemeContext.use(h => {
 	/**
-	 * Draggable divider component to split windows into two. 
-     * The parent div should have flex display.
-     * The Divider Component should be in between the two sibling windows components within the parent div 
-     * The left window div should have style={{width: leftWindowPercentage.map(w => `${w * 100}%`) }}
-     * The right window div should have style={{width: leftWindowPercentage.map(w => `${100 - w * 100}%`) }}
-     * 
+	 * Draggable divider component to split windows into two.
+	 * The parent div should have flex display.
+	 * The Divider Component should be in between the two sibling windows components within the parent div
+	 * The left window div should have style={{width: leftWindowPercentage.map(w => `${w * 100}%`) }}
+	 * The right window div should have style={{width: leftWindowPercentage.map(w => `${100 - w * 100}%`) }}
+	 *
 	 * @param {Object} props - The properties object.
-     * @param {Observer<number>} [props.leftWindowPercentage] - Observable number from 0 to 1 to determine tiling window sizes on render and during dragging.
-     * @param {int} [props.leftOffset=0] - Number of pixels on the left of the page taken up by navigation bars, menu, etc.
-     * @param {number} [props.leftMin=20] - Number from 0 to 1 representing the minimum width percentage taken up by the left tile.
-     * @param {number} [props.leftMax=80] - Number from 0 to 1 representing the maximum width percentage taken up by the left tile.
+	 * @param {Observer<number>} [props.leftWindowPercentage] - Observable number from 0 to 1 to determine tiling window sizes on render and during dragging.
+	 * @param {int} [props.leftOffset=0] - Number of pixels on the left of the page taken up by navigation bars, menu, etc.
+	 * @param {number} [props.leftMin=20] - Number from 0 to 1 representing the minimum width percentage taken up by the left tile.
+	 * @param {number} [props.leftMax=80] - Number from 0 to 1 representing the maximum width percentage taken up by the left tile.
 	 * @param {Object} [props.style] - Custom styles to apply to the button.
 	 *
 	 * @returns {JSX.Element} The rendered draggable window divider element.
 	 */
 	const Divider = ({
-        leftWindowPercentage,
-        leftOffset = 0,
-        leftMin = 0.2,
-        leftMax = 0.8,
-        style
-    }, cleanup) => {
-        if (!(leftWindowPercentage instanceof Observer)) leftWindowPercentage = Observer.mutable(0.5);
-        let resizingWindow = false;
-        const pageWidth = (window.innerWidth - leftOffset);
+		leftWindowPercentage,
+		leftOffset = 0,
+		leftMin = 0.2,
+		leftMax = 0.8,
+		style
+	}, cleanup) => {
+		if (!(leftWindowPercentage instanceof Observer)) leftWindowPercentage = Observer.mutable(0.5);
 
-        const handleMouseDown = () => {
-            resizingWindow = true;
-            document.addEventListener('mousemove', handleMouseMove);
-            document.addEventListener('mouseup', handleMouseUp);
-        };
+		let resizingWindow = false;
+		const pageWidth = (window.innerWidth - leftOffset);
 
-        const handleMouseMove = (e) => {
-            if (!resizingWindow) return;
-            if (e.clientX  < pageWidth * leftMin + leftOffset || e.clientX> pageWidth * leftMax + leftOffset) return;
+		const handleMouseDown = () => {
+			resizingWindow = true;
+			document.addEventListener('mousemove', handleMouseMove);
+			document.addEventListener('mouseup', handleMouseUp);
+		};
 
-            leftWindowPercentage.set((e.clientX - leftOffset)/ pageWidth );
-        };
+		const handleMouseMove = (e) => {
+			if (!resizingWindow) return;
+			if (e.clientX < pageWidth * leftMin + leftOffset || e.clientX > pageWidth * leftMax + leftOffset) return;
 
-        const handleMouseUp = () => {
-            resizingWindow = false;
-            document.removeEventListener('mousemove', handleMouseMove);
-            document.removeEventListener('mouseup', handleMouseUp);
-        };
+			leftWindowPercentage.set((e.clientX - leftOffset) / pageWidth);
+		};
 
-        cleanup(() => {
-            document.removeEventListener('mousemove', handleMouseMove);
-            document.removeEventListener('mouseup', handleMouseUp);
-        });
+		const handleMouseUp = () => {
+			resizingWindow = false;
+			document.removeEventListener('mousemove', handleMouseMove);
+			document.removeEventListener('mouseup', handleMouseUp);
+		};
 
-        return<div theme='divider' style={style} onMouseDown={handleMouseDown}></div>;
-    };
+		cleanup(() => {
+			if (!resizingWindow) return;
+
+			document.removeEventListener('mousemove', handleMouseMove);
+			document.removeEventListener('mouseup', handleMouseUp);
+		});
+
+		return <div theme='divider' style={style} onMouseDown={handleMouseDown} />;
+	};
 
 	return Divider;
 });
