6393e8d563f40e9b7d39c1b1ba23295dd4229b57
Alexander Orzechowski <alex@ozal.ski>
2025-01-15 12:37:18 -0500

    Detached: Hide detached popup if contents move

--- components/utils/Detached.jsx
+++ components/utils/Detached.jsx
@@ -97,6 +97,29 @@ const Detached = ThemeContext.use(h => {
 				focused.set(false);
 			} else {
 				focused.set(fits[0].rot);
+
+				let checking = true;
+				const check = () => {
+					if (checking) {
+						const updated = getBounds(elems);
+
+						if (surround.left !== updated.left ||
+								surround.right !== updated.right ||
+								surround.top !== updated.top ||
+								surround.bottom !== updated.bottom) {
+							focused.set(false);
+							return;
+						}
+
+						window.requestAnimationFrame(check);
+					}
+				};
+				window.requestAnimationFrame(check);
+
+				// poll the bounds for changes - if something changes we will hide the modal
+				return () => {
+					checking = false;
+				};
 			}
 		}));
 
