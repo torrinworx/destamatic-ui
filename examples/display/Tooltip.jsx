a1b9603240364805dc36268f75590670011dbfdb
Alexander Orzechowski <alex@ozal.ski>
2025-01-20 22:15:13 -0500

    examples/tooltip: expand

--- examples/display/Tooltip.jsx
+++ examples/display/Tooltip.jsx
@@ -1,11 +1,47 @@
-import { Tooltip, popups } from 'destamatic-ui';
+import { Tooltip, popups, Detached } from 'destamatic-ui';
 import { mount, OArray, Observer } from 'destam-dom';
 
-mount(document.body, <>
-	<div theme="center" style={{inset: 0}}>
-		<Tooltip label="This is a tooltip">
-			<div>This is some text</div>
+const Comp = ({style}) => {
+	return <div theme="center" style={{position: 'absolute', inset: 0, flexDirection: 'column', gap: 40, pointerEvents: 'none', ...style}}>
+		<Tooltip label="show tooltip below" locations={[Detached.BOTTOM_CENTER]}>
+			<div style={{pointerEvents: 'auto'}}>This is some text</div>
+		</Tooltip>
+
+		<Tooltip label="show tooltip above" locations={[Detached.TOP_CENTER]}>
+			<div style={{pointerEvents: 'auto'}}>This is some text</div>
+		</Tooltip>
+
+		<Tooltip label="show tooltip to the right" locations={[Detached.RIGHT_CENTER]}>
+			<div style={{pointerEvents: 'auto'}}>This is some text</div>
+		</Tooltip>
+
+		<Tooltip label="show tooltip to the left" locations={[Detached.LEFT_CENTER]}>
+			<div style={{pointerEvents: 'auto'}}>This is some text</div>
 		</Tooltip>
 	</div>
+};
+
+mount(document.body, <>
+	<Comp />
+	<Comp style={{alignItems: 'start'}} />
+	<Comp style={{alignItems: 'end'}} />
+	<Tooltip label="show tooltip">
+		<div style={{position: 'absolute'}}>This is some text</div>
+	</Tooltip>
+	<Tooltip label="show tooltip">
+		<div style={{position: 'absolute', right: 0}}>This is some text</div>
+	</Tooltip>
+	<Tooltip label="show tooltip">
+		<div style={{position: 'absolute', bottom: 0}}>This is some text</div>
+	</Tooltip>
+	<Tooltip label="show tooltip">
+		<div style={{position: 'absolute', bottom: 0, right: 0}}>This is some text</div>
+	</Tooltip>
+	<Tooltip label="show tooltip">
+		<div style={{position: 'absolute', bottom: 100, right: -70, width: 100, height: 10, background: 'red'}}/>
+	</Tooltip>
+	<Tooltip label="show tooltip">
+		<div style={{position: 'absolute', bottom: 0, left: 300, height: 100, width: 10, background: 'red'}}/>
+	</Tooltip>
 	{popups}
 </>);
