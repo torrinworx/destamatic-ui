559ddce309587b3e7f05ea4c6a636117210b0d06
torrin1 <torrin@worx4you.com>
2025-01-23 10:29:58 -0500

    Removing need for editable field in Typography

--- components/display/Typography.jsx
+++ components/display/Typography.jsx
@@ -26,35 +26,29 @@ Theme.define({
 /**
  * Creates a Typography element for rendering styled text with optional editing capabilities.
  *
- * - Inline editing if `editable` is `true` and the `label` is a mutable Observer.
- * - The `editable` parameter allows toggling between an editable field and a dynamically updating
- *   text element. Useful for scenarios where the text should update in real-time but remain non-editable.
- *
  * @function
  * @param {Object} options - Configuration options for the Typography element.
  * @param {string | string[]} [options.type='h1'] - The typography type (e.g., 'h1', 'h2', 'p1') and aditional styling (e.g., 'bold', 'italic', 'inline', 'center').
  * @param {string | Observer} [options.label=''] - The text content or an Observer wrapping it.
- * @param {boolean} [options.editable=false] - Controls whether the text can be edited.
  * @param {string[]} [options.children] - Nested text or elements inside the typography container.
  * @param {Object} [options.props] - Additional attributes for the container.
  * @returns {HTMLElement} The constructed Typography element.
  */
 export default ThemeContext.use(h => {
-	const Typography = ({ type = 'h1', label = '', editable = false, children, ...props }) => {
-		if (!(editable instanceof Observer)) editable = Observer.mutable(editable);
+	const Typography = ({ type = 'h1', label = '', children, ...props }) => {
 		if (!(label instanceof Observer)) label = Observer.immutable(label);
 		const isEditing = Observer.mutable(false);
 
 		return <div
+			{...props}
+			theme={['typography', ...Array.isArray(type) ? type : type.split('_')]}
 			onClick={(e) => {
 				if (!label.isImmutable() && e.detail === 2) {
 					isEditing.set(true);
 				}
 			}}
-			{...props}
-			theme={['typography', ...Array.isArray(type) ? type : type.split('_')]}
 		>
-			<Shown value={Observer.all([isEditing, editable]).map(([i, e]) => i && e)}>
+			<Shown value={Observer.all([isEditing, label]).map(([i, l]) => i && !label.isImmutable())}>
 				<mark:then>
 					<TextField
 						value={label}
