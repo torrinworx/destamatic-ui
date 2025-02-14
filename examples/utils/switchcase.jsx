62bb652859101b4fc366b33a9c4cc3add7e0a257
Alexander Orzechowski <alex@ozal.ski>
2025-02-14 18:33:15 -0500

    tests: Add switchcase example for cases parameter

--- examples/utils/switchcase.jsx
+++ examples/utils/switchcase.jsx
@@ -1,7 +1,11 @@
 import { mount, Observer} from 'destam-dom';
-import { SwitchCase, Button } from 'destamatic-ui';
+import { SwitchCase, Button, Toggle } from 'destamatic-ui';
 
 const value = Observer.mutable('red');
+
+const one = Observer.mutable(false);
+const two = Observer.mutable(false);
+const three = Observer.mutable(false);
 
 mount(document.body, <>
 	<SwitchCase value={value} >
@@ -22,4 +26,25 @@ mount(document.body, <>
 	<Button label="click me" type='contained' style={{backgroundColor: 'green'}} onClick={() => value.set('green')} />
 	<Button label="no me" type='outlined' style={{backgroundColor: 'blue'}}  onClick={() => value.set('blue')} />
 	<Button label="meeeee" type='outlined' style={{backgroundColor: 'pink'}}  onClick={() => value.set('pink')} />
+
+     <Toggle value={one} />
+     <Toggle value={two} />
+     <Toggle value={three} />
+
+     <SwitchCase cases={{one, two, three}}>
+       <mark:case value='one'>
+          The first toggle is on and trumps the state of all the others defined later
+       </mark:case>
+       <mark:case value='two'>
+          The second toggle is on and the first toggle is off
+       </mark:case>
+       <mark:case value='three'>
+          The first toggle is off, the second toggle is off, and the third toggle is on.
+       </mark:case>
+       <mark:default>
+            No toggles are switched
+       </mark:default>
+    </SwitchCase>
+
+
 </>);
