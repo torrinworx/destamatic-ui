9609d2ea910dd4229d357c86cd2a94732e13f990
torrin1 <torrin@worx4you.com>
2025-06-15 20:15:06 -0400

    Renaming modals to stage since we can use it to create a page system and much more. Fixing default template for modals/stages, adding a page system to the modal example file

---
+++ components/display/Stage.jsx
@@ -0,0 +1,138 @@
+import { OObject, Observer } from 'destam';
+
+import Icon from './Icon';
+import Popup from '../utils/Popup';
+import Theme from '../utils/Theme';
+import Paper from './Paper';
+import Button from '../inputs/Button';
+import Typography from './Typography';
+import createContext from '../utils/Context';
+import ThemeContext from '../utils/ThemeContext';
+
+Theme.define({
+	stageOverlay: {
+		background: 'rgba(0, 0, 0, 0.7)',
+		height: '100vh',
+		width: '100vw'
+	},
+	stageWrapper: {
+		top: '50%',
+		left: '50%',
+		position: 'absolute',
+		transform: 'translate(-50%, -50%)',
+	}
+});
+
+const DefaultTemplate = ThemeContext.use(h => ({ s, closeSignal, children }, cleanup, mounted) => {
+	const shown = Observer.mutable(false);
+
+	const handleEscape = (e) => {
+		if (e.which === 27) {
+			e.preventDefault();
+			s.current = false;
+		}
+	};
+
+	mounted(() => {
+		queueMicrotask(() => {
+			setTimeout(() => shown.set(true), 10);
+		});
+		if (!s.props.noEsc) {
+			window.addEventListener('keydown', handleEscape);
+			cleanup(() => {
+				window.removeEventListener('keydown', handleEscape);
+			});
+		}
+	});
+
+	cleanup(closeSignal.watch(() => {
+		shown.set(!closeSignal.get());
+	}));
+
+	return <Popup style={{
+		inset: 0,
+		transition: `opacity ${s.currentDelay}ms ease-in-out`,
+		opacity: shown.map(shown => shown ? 1 : 0),
+		pointerEvents: shown.map(shown => shown ? null : 'none'),
+	}}>
+		<div theme='stageOverlay'
+			onClick={() => !s.props.noClickEsc ? s.close() : null} />
+		<div theme='stageWrapper'>
+			<Paper>
+				<div theme='row_spread'>
+					<Typography
+						type='h2'
+						label={s.observer.path(['props', 'label'])
+							.map(l => l ? l : '')}
+					/>
+					<Button
+						type='icon'
+						icon={<Icon name='x' size={30} />}
+						onClick={() => s.close()}
+					/>
+				</div>
+				{children}
+			</Paper>
+		</div>
+	</Popup>;
+});
+
+export const StageContext = createContext(() => null, (value) => {
+	const { stages, template: defaultTemplate = DefaultTemplate, ...globalProps } = value;
+
+	const Stage = OObject({
+		stages,
+		template: defaultTemplate,
+		open: ({ name, template = Stage.template, ...props }) => {
+			Stage.props = { ...globalProps, ...props };
+			Stage.template = template;
+			Stage.current = name;
+		},
+		close: () => {
+			Stage.current = null;
+		},
+		cleanup: () => {
+			Stage.template = defaultTemplate;
+		},
+		current: null,
+		currentDelay: 150,
+	});
+
+	return Stage;
+});
+
+export const Stage = StageContext.use(s => ThemeContext.use(h => (_, cleanup) => {
+	const aniCurrent = Observer.mutable(null);
+	cleanup(s.observer.path('current').effect(current => {
+		if (current) aniCurrent.set(current);
+		else {
+			const timeout = setTimeout(() => {
+				s.cleanup();
+				aniCurrent.set(null);
+			}, s.currentDelay);
+
+			return () => clearTimeout(timeout);
+		}
+	}));
+
+	return Observer.all([s.observer.path('template'), aniCurrent])
+		.map(([Template, c]) => {
+			if (!c) return null;
+
+			const closeSignal = s.observer.path('current').map(current => {
+				if (!current) return true;
+				else return current !== c;
+			});
+
+			let Stage = null;
+			if (s && s.stages && typeof c === 'string' && c in s.stages) {
+				Stage = s.stages[c];
+			} else {
+				console.error(`Stage with '${c}' does not exist in stages list.`);
+			}
+
+			return <Template closeSignal={closeSignal} s={s}>
+				<Stage stage={s} {...s.props} />
+			</Template>;
+		});
+}));
---
+++ examples/display/stage.example.jsx
@@ -0,0 +1,194 @@
+import { mount, Observer } from 'destam-dom';
+import FeatherIcons from "destamatic-ui/components/icons/FeatherIcons";
+import { Button, Stage, StageContext, popups, TextField, Typography, Icons, Popup, Paper } from 'destamatic-ui';
+
+const pages = {
+    stages: {
+        Home: ({ }) => {
+            return <div style={{ background: 'red' }}>
+                Home page content
+            </div>;
+        },
+        About: ({ }) => {
+            return <div style={{ background: 'blue' }}>
+                About page content
+            </div>;
+        },
+        Team: ({ }) => {
+            return <div style={{ background: 'green' }}>
+                Team page content
+            </div>;
+        },
+        Careers: ({ }) => {
+            return <div style={{ background: 'orange' }}>
+                Careers page content
+            </div>;
+        },
+    },
+    template: ({ s, children }) => {
+        return <div style={{ width: '100%', height: '100%', background: 'purple' }}>
+            <div theme='row_spread'>
+                <Typography
+                    type='h2'
+                    label={s.observer.path(['props', 'label'])
+                        .map(l => l ? l : '')}
+                />
+            </div>
+            {children}
+        </div>;
+    }
+};
+
+const Pages = StageContext.use(s => (_,__, mounted) => {
+    const buttons = [
+        {
+            label: 'Home',
+            onClick: () => s.open({ name: 'Home', label: 'Home' })
+        },
+        {
+            label: 'About',
+            onClick: () => s.open({ name: 'About', label: 'About' })
+        },
+        {
+            label: 'Team',
+            onClick: () => s.open({ name: 'Team', label: 'Team' })
+        },
+        {
+            label: 'Careers',
+            onClick: () => s.open({ name: 'Careers', label: 'Careers' })
+        }
+    ];
+
+    const Buttons = ({ each }) => <Button type='contained' label={each.label} onClick={each.onClick} />
+
+    mounted(() => {
+        s.open({ name: 'Home', label: 'Home' });
+    });
+
+    return <div>
+        <Typography type='h2' label='Pages' />
+        <Typography type='p1' label='Stages can be used to create a page system given a list of components.' />
+        <div theme='row' style={{ height: 500 }}>
+            <div theme='column' style={{ width: 100 }}>
+                <Buttons each={buttons} />
+            </div>
+            <Stage />
+        </div>
+    </div>;
+});
+
+const timer = Observer.timer(1000);
+
+const modals = {
+    stages: {
+        // basic modal
+        basic: ({ }) => {
+            return <div style={{ width: '500px', height: '500px', background: 'red' }}>
+                test
+            </div>;
+        },
+
+        // modal with context from where it was invoked.
+        state: ({ timer, someContext }) => {
+            return <div style={{ width: '500px', height: '500px', background: 'blue' }}>
+                {someContext ?? null}
+                <br />
+                Global state passed into modal from context declaration:
+                <br />
+                {timer}
+                <br />
+            </div>;
+        },
+
+        // disable click away and esc button
+        trapped: ({ stage }) => {
+            return <div style={{ width: '500px', height: '500px', background: 'green' }}>
+                you can't escape!
+                <Button type='contained' label='Exit' onClick={() => stage.close()} />
+            </div>;
+        },
+
+        // cross modal context
+        page1: ({ stage }) => {
+            const val = Observer.mutable();
+
+            return <div style={{ width: '500px', height: '500px', background: 'blue' }}>
+                Welcome to Page 1.
+                <br />
+                clicking this button will move you to page2 without losing context
+
+                <TextField placholder='Enter some context' value={val} />
+                <Button type='contained' label='page2' onClick={() => {
+                    stage.open({ name: 'page2', label: 'Page 2', val })
+                }} />
+            </div>
+        },
+        page2: ({ val }) => {
+            return <div style={{ width: '500px', height: '500px', background: 'green' }}>
+                Here is your context from page1!!!
+                <br />
+                {val}
+            </div>;
+        },
+
+        // Custom modal template:
+        template: ({ }) => {
+            return <div style={{ width: '500px', height: '500px', background: 'orange' }}>
+                Custom template!
+            </div>;
+        }
+    },
+    // place any state you want into value when sending it to ModalContext, it will get sent to the modal automatically.
+    timer,
+};
+
+const Modals = StageContext.use(s => () => <>
+    <Typography type='h3' label='Modals' />
+    <Typography type='p1' label='Stages can also be used to create modals in combination with the Popup component. This is the Stage systems default template.' />
+
+    <Button type='contained' label='Basic' onClick={() => {
+        s.open({ name: 'basic', label: 'Basic Modal' })
+    }} />
+    <Button type='contained' label='State' onClick={() => {
+        s.open({ name: 'state', someContext: 'some extra context here', label: 'Modal with state' });
+    }} />
+    <Button type='contained' label='Trapped' onClick={() => {
+        s.open({ name: 'trapped', label: 'Trapped!', noEsc: true, noClickEsc: true });
+    }} />
+    <Button type='contained' label='Paged' onClick={() => {
+        s.open({ name: 'page1', label: 'Page 1' });
+    }} />
+    <Button type='contained' label='Template' onClick={() => {
+        const customTemplate = ({ s, children }) => {
+            console.log(s);
+            return <Popup style={{ inset: 0 }}>
+                <div theme='stageOverlay' onClick={() => !s.noClickEsc ? s.close() : null} />
+                <div theme='stageWrapper' style={{ background: 'purple' }}>
+                    <Typography
+                        type='p1'
+                        label={`this is the label: ${s.label}`}
+                    />
+                    These are the child elements:
+                    {children}
+                </div>
+            </Popup>;
+        };
+
+        s.open({ name: 'template', template: customTemplate, label: 'Template' });
+    }} />
+</>);
+
+mount(document.body, <div>
+    <Icons value={[FeatherIcons]}>
+        <Typography type='h1' label='Stage' />
+        <Typography type='p1' label='A Stage allows you to display, transition between, template, and control different components.' />
+        <StageContext value={pages}>
+            <Pages />
+        </StageContext>
+        <StageContext value={modals}>
+            <Modals />
+            <Stage />
+            {popups}
+        </StageContext>
+    </Icons>
+</div>);
