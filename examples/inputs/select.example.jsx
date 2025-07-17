89dd19a3487b3c1b7e9d7dfb69d6cfa370de4884
torrin1 <torrin@worx4you.com>
2025-07-17 15:37:08 -0400

    Updating Select example

--- examples/inputs/select.example.jsx
+++ examples/inputs/select.example.jsx
@@ -1,13 +1,28 @@
 import { Observer, mount } from 'destam-dom';
-import { Select, popups, Typography } from 'destamatic-ui';
+import { Select, Typography, PopupContext, Popup } from 'destamatic-ui';
 
 const input = Observer.mutable('');
 
-mount(document.body, <>
-	<div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', inset: 0, position: 'absolute', flexDirection: 'column'}}>
+const options = {
+	'option 1': () => {
+		window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_self');
+	},
+	'option 1': () => {
+		window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_self');
+	},
+	'option 1': () => {
+		window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_self');
+	},
+	'option 1': () => {
+		window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_self');
+	},
+};
+
+mount(document.body, <PopupContext>
+	<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', inset: 0, position: 'absolute', flexDirection: 'column' }}>
 		<Typography label={input} />
-		<Select options={['one', 'two', 'three']} style={{width: '100px'}} type="contained" value={Observer.mutable('one')} />
-		<Select options={Array(70).fill(null).map((_, i) => String.fromCharCode(i + 41))} style={{width: '100px'}} type="contained" value={Observer.mutable('a')} />
+		<Select options={['one', 'two', 'three']} style={{ width: '100px' }} type="contained" value={Observer.mutable('one')} />
+		<Select options={Array(70).fill(null).map((_, i) => String.fromCharCode(i + 41))} style={{ width: '100px' }} type="contained" value={Observer.mutable('a')} />
 	</div>
 	{[
 		['left', 'top'],
@@ -15,8 +30,17 @@ mount(document.body, <>
 		['left', 'bottom'],
 		['right', 'bottom'],
 	].map(([a, b]) => {
-		return <Select style={{position: 'absolute', [a]: 0, [b]: 0}} value={input} options={Array(100).fill(null).map((_, i) => i)} />
+		return <Select style={{ position: 'absolute', [a]: 0, [b]: 0 }} value={input} options={Array(100).fill(null).map((_, i) => i)} />
 	})}
 
-	{popups}
-</>);
+	{/* You can use select to create a static dropdown with each button running a function, not just a selectable list */}
+	<Select
+		placeholder='Functions'
+		options={Object.keys(options)}
+		value={Observer.immutable().setter(key => options[key]())}
+		style={{ width: 150, border: 'none' }}
+		type='text'
+	/>
+
+	<Popup />
+</PopupContext>);
