6da5367fcffe8e67744a3e141ff11a169d0d370a
Alexander Orzechowski <alex@ozal.ski>
2025-07-18 13:48:04 -0400

    util/categories: Record all props

--- util/categories.js
+++ util/categories.js
@@ -5,6 +5,7 @@ export default (children, categories, def) => {
 	const maps = new Map();
 	for (const cat of categories) {
 		const arr = [];
+		arr.props = {};
 		out.push(arr);
 		maps.set(cat, arr);
 	}
@@ -16,7 +17,13 @@ export default (children, categories, def) => {
 				throw new Error("Element accepts the following marks: " + categories.join(", "));
 			}
 
-			arr.push(...child.props.children);
+			for (let o in child.props) {
+				if (o === 'children') {
+					arr.push(...child.props.children);
+				} else {
+					arr.props[o] = child.props[o];
+				}
+			}
 		} else if (def) {
 			maps.get(def).push(child);
 		} else {
