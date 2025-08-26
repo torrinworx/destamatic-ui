fd19e24e53632f265a20b4ab95809f16629b880a
torrin1 <torrin@worx4you.com>
2025-08-26 11:31:42 -0400

    Fixing gradiant styling

--- components/utils/Gradient.jsx
+++ components/utils/Gradient.jsx
@@ -33,11 +33,7 @@ Theme.define({
 		transition: 'opacity 250ms ease-in-out',
 	},
 	gradientContent: {
-		position: 'fixed',
-		top: 0,
-		left: 0,
-		right: 0,
-		bottom: 0,
+		position: 'relative',
 		overflowY: 'auto',
 	},
 });
