0f1b7ceb2cb5dae9fc8ca14fcaad79890839aca0
Alexander Orzechowski <alex@ozal.ski>
2025-10-31 12:32:22 -0400

    defaultTheme: Also transition fill colors

--- util/defaultTheme.js
+++ util/defaultTheme.js
@@ -28,7 +28,7 @@ export default {
 	"*": {
 		fontFamily: 'Roboto, sans-serif',
 		boxSizing: 'border-box',
-		transition: 'opacity 250ms ease-out, box-shadow 250ms ease-out, background-color 250ms ease-in-out, color 250ms ease-in-out',
+		transition: 'opacity 250ms ease-out, box-shadow 250ms ease-out, background-color 250ms ease-in-out, color 250ms ease-in-out, fill 250ms ease-in-out',
 
 		/*
 		Adjusts the brightness of the input colour by shifting its value in the HSV colour space. Accepts colours
