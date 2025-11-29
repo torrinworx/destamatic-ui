47a849524b19514b65e50fe13e3ac382b37edcb1
Torrin Leonard <torrin@torrin.me>
2025-11-28 19:45:38 -0500

    Updating build.js to render stage tree structures

--- ssg/build.js
+++ ssg/build.js
@@ -6,30 +6,28 @@ import './document.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const OUT_DIR = path.resolve(__dirname, '../../build/dist');
+const OUT_DIR = path.resolve(__dirname, '../../build/dist'); // TODO: .env
 
 const build = async () => {
-	const { renderAppToString } = await import('../../build/server/ssg-entry.js');
-	const pages = renderAppToString();
+  const { renderAppToString } = await import('../../build/server/ssg-entry.js'); // TODO: .env
+  const pages = renderAppToString();
 
-	for (const page of pages) {
-		const { route, name, html } = page; // TODO: minifi html before? 
+  for (const page of pages) {
+    const { route, name, html } = page; // route: folder, name: file base
+    // route may be '/', '/blogs/', etc.
+    const relRoute = route === '/' ? '' : route.replace(/^\/|\/$/g, '');
+    const dir = relRoute ? path.join(OUT_DIR, relRoute) : OUT_DIR;
 
-		await fs.mkdir(path.join(OUT_DIR, route), { recursive: true });
+    await fs.mkdir(dir, { recursive: true });
 
-		console.log("WRITING:", route, name + '.html');
+    const filePath = path.join(dir, name + '.html');
+    await fs.writeFile(filePath, html, 'utf8');
 
-		await fs.writeFile(
-			path.join(OUT_DIR, route, name + '.html'),
-			html,
-			'utf8'
-		);
+    console.log('SSG: wrote', filePath);
+  }
+};
 
-		console.log('SSG: wrote', path.join(OUT_DIR, route, name + '.html'));
-	}
-}
-
-build().catch(err => {
-	console.error(err);
-	process.exit(1);
+build().catch((err) => {
+  console.error(err);
+  process.exit(1);
 });
