a893095e37a0c7fc28bbf21d140be1d8c88f72cc
Torrin Leonard <torrin@torrin.me>
2025-12-15 15:52:30 -0500

    updating example styles

--- examples/inputs/richarea.example.jsx
+++ examples/inputs/richarea.example.jsx
@@ -132,10 +132,10 @@ mount(document.body, <PopupContext>
 			}
 		}
 	]} >
-		<RichArea value={value} type='h1' maxHeight={1000} />
+		<RichArea value={value} type='p1' maxHeight={1000} />
 	</TextModifiers>
 	<Typography label="Javascript syntax highlighting:" type='h1' />
 	<TextModifiers value={modifiers} >
-		<RichArea value={value2} type='h1' maxHeight={1000} />
+		<RichArea value={value2} type='p1' maxHeight={1000} />
 	</TextModifiers>
 </PopupContext>);
