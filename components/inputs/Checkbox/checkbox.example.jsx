236ab187e46f710891c603d2e487857cb873c35a
Torrin Leonard <torrin@torrin.me>
2025-12-16 23:40:10 -0500

    Simplifying checkbox exampl

--- components/inputs/Checkbox/checkbox.example.jsx
+++ components/inputs/Checkbox/checkbox.example.jsx
@@ -1,35 +1,15 @@
 import { Observer } from 'destam';
 import { mount } from 'destam-dom';
-import { Checkbox, Typography } from 'destamatic-ui';
+import { Checkbox } from 'destamatic-ui';
 
 const Example = () => {
-	const checkboxCount = Observer.mutable(0);
+	const checked = Observer.mutable(false);
 
 	return <div theme='column_center'>
-		<Typography
-			type='p1'
-			label={checkboxCount.map(c => `Boxes checked: ${c}`)}
-		/>
+		{checked.map(c => `Checked: ${c}`)}
 
 		<div theme='row'>
-			{Array.from({ length: 10 }).map(() => Observer.mutable(false)).map(box =>
-				<Checkbox
-					value={box}
-					onChange={val => {
-						if (val) {
-							checkboxCount.set(checkboxCount.get() + 1);
-							setTimeout(() => {
-								if (box.get()) {
-									box.set(false);
-									checkboxCount.set(checkboxCount.get() - 1);
-								}
-							}, 2500);
-						} else {
-							checkboxCount.set(checkboxCount.get() - 1);
-						}
-					}}
-				/>
-			)}
+			<Checkbox value={checked} />
 		</div>
 	</div>
 };
