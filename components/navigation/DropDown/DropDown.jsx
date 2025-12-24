97384c3bd1391d27223c9cb15c46a92ae801c1c0
Torrin Leonard <torrin@torrin.me>
2025-12-24 14:42:09 -0500

    DropDown.jsx: Fixing focused state, utilizing button to standardize api use.

--- components/navigation/DropDown/DropDown.jsx
+++ components/navigation/DropDown/DropDown.jsx
@@ -4,97 +4,68 @@ import { h } from '../../utils/h/h.jsx';
 import Theme from '../../utils/Theme/Theme.jsx';
 import ThemeContext from '../../utils/ThemeContext/ThemeContext.jsx';
 
+import Button from '../../inputs/Button/Button.jsx';
 import Shown from '../../utils/Shown/Shown.jsx';
-import useRipples from '../../utils/Ripple/Ripple.jsx';
 import { Icon } from '../../display/Icon/Icon.jsx';
 
 Theme.define({
 	dropdown: {
-		extends: 'center_radius',
-		padding: 10,
-		userSelect: 'none',
-		cursor: 'pointer',
-		position: 'relative',
-		overflow: 'clip',
-		background: 'none',
-		border: 'none',
-		_cssProp_focus: { outline: 'none' },
-	},
-
-	dropdown_hovered: {
-		background: 'rgb(0, 0, 0, 0.1)',
-	},
-
-	dropdown_row: {
-		extends: 'row_fill_spread',
-	},
+		extends: 'text_fill_spread',
+		maxWidth: 'none',
+	}
 });
 
 export default ThemeContext.use(h => {
 	const DropDown = ({
-		children,
-		label,
 		arrow = 'right',
-		style,
 		open = Observer.mutable(false),
 		iconOpen = <Icon size="20" name="chevron-down" />,
 		iconClose = <Icon size="20" name="chevron-down" />,
+		label = '',
+		type = 'dropdown',
+		onClick,
+		inline,
+		onMouseDown,
+		onMouseUp,
+		icon = null,
+		style,
+		disabled,
 		hover,
 		focused,
+		children,
+		iconPosition = 'left',
+		loading = true,
+		href,
 		...props
 	}) => {
 		if (!(open instanceof Observer)) open = Observer.mutable(open);
-		if (!(focused instanceof Observer)) focused = Observer.mutable(false);
-		if (!(hover instanceof Observer)) hover = Observer.mutable(false);
-
-		const toggle = () => open.set(!open.get());
-		const [ripples, createRipple] = useRipples();
-
-		if (!focused.isImmutable()) props.isFocused = focused;
-		if (!hover.isImmutable()) props.isHovered = hover;
+		if (!(focused instanceof Observer)) focused = Observer.mutable(focused);
+		if (!(hover instanceof Observer)) hover = Observer.mutable(hover);
 
 		return <div style={style}>
-			<div
-				ref
+			<Button
+				type={type}
+				iconPosition={arrow === 'right' ? 'right' : 'left'}
+				label={label}
+				icon={open.map(show => (show ? iconOpen : iconClose))}
+				onClick={(event) => {
+					open.set(!open.get());
+					if (onClick) onClick(event);
+				}}
+				inline={inline}
+				onMouseDown={onMouseDown}
+				onMouseUp={onMouseUp}
+				disabled={disabled}
+				hover={hover}
+				focused={focused}
+				loading={loading}
+				href={href}
 				{...props}
-				theme={[
-					'dropdown',
-					hover.bool('hovered', null),
-					focused.bool('focused', null),
-				]}
-				onMouseEnter={() => hover.set(true)}
-				onMouseLeave={() => {
-					hover.set(false);
-					focused.set(false);
-				}}
-				onMouseDown={(event) => {
-					focused.set(true);
-					createRipple(event);
-					toggle();
-				}}
-				onKeyDown={(event) => {
-					if (event.key === 'Enter' || event.key === ' ') {
-						event.preventDefault();
-						focused.set(true);
-						createRipple(event);
-						toggle();
-					}
-				}}
-			>
-				<div theme={['row_fill_spread']}>
-					{arrow === 'right' ? <span>{label}</span> : null}
+			/>
 
-					<i class="chevron-icon" style={{ display: 'flex', alignItems: 'center' }}>
-						{open.map(show => (show ? iconOpen : iconClose))}
-					</i>
-
-					{arrow === 'left' ? <span>{label}</span> : null}
-				</div>
-
-				{ripples}
-			</div>
-
-			<Shown value={open} children={children} />
+			<Shown value={open}>
+				{children}
+			</Shown>
 		</div>;
 	};
 
