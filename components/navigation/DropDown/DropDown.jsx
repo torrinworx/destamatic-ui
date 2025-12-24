460cf003e55517631cecec819af973a79a3a0ac7
Torrin Leonard <torrin@torrin.me>
2025-12-24 12:21:38 -0500

    DropDown.jsx: Theme and style upgrades to dropdown, general organizational improvements.

--- components/navigation/DropDown/DropDown.jsx
+++ components/navigation/DropDown/DropDown.jsx
@@ -1,60 +1,102 @@
 import { Observer } from 'destam-dom';
 
 import { h } from '../../utils/h/h.jsx';
+import Theme from '../../utils/Theme/Theme.jsx';
+import ThemeContext from '../../utils/ThemeContext/ThemeContext.jsx';
+
 import Shown from '../../utils/Shown/Shown.jsx';
+import useRipples from '../../utils/Ripple/Ripple.jsx';
 import { Icon } from '../../display/Icon/Icon.jsx';
 
-/**
- * DropDown component that toggles the visibility of its children when clicked.
- * 
- * The component displays a label and an arrow icon (either to the right or the left).
- * Clicking the label or the arrow toggles the visibility of the children components.
- * 
- * @param {Object} props - The properties object.
- * @param {JSX.Element | Array<JSX.Element>} props.children - The content to be displayed inside the dropdown when expanded.
- * @param {string} props.label - The label to be displayed next to the dropdown arrow.
- * @param {string} [props.arrow='right'] - Determines the position of the arrow relative to the label. Can be 'right' or 'left'.
- * 
- * @returns {JSX.Element} The rendered dropdown component.
- */
-const DropDown = ({
-    children,
-    label,
-    arrow = 'right',
-    style,
-    open = Observer.mutable(false),
-    iconOpen = <Icon size='20' name='chevron-down' />,
-    iconClose = <Icon size='20' name='chevron-down' />,
-}) => {
-    if (!(open instanceof Observer)) open = Observer.mutable(open);
+Theme.define({
+	dropdown: {
+		extends: 'center_radius',
+		padding: 10,
+		userSelect: 'none',
+		cursor: 'pointer',
+		position: 'relative',
+		overflow: 'clip',
+		background: 'none',
+		border: 'none',
+		_cssProp_focus: { outline: 'none' },
+	},
 
-    const toggle = () => open.set(!open.get());
+	dropdown_hovered: {
+		background: 'rgb(0, 0, 0, 0.1)',
+	},
 
-    return <div style={style}>
-        <div
-            style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: arrow === 'right' ? 'space-between' : 'flex-start',
-                cursor: 'pointer',
-                userSelect: 'none'
-            }}
-            onMouseDown={toggle}>
-            {arrow === 'right' ? <span>{label}</span> : null}
-            <div style={{ display: 'flex', alignItems: 'center', userSelect: 'none' }}>
-                <i class='chevron-icon' style={{ cursor: 'pointer' }}>
-                    {open.map((show) => show ? (
-                        iconOpen
-                    ) : (
-                        iconClose
-                    ))}
-                </i>
-            </div>
-            {arrow === 'left' ? <span>{label}</span> : null}
-        </div>
+	dropdown_row: {
+		extends: 'row_fill_spread',
+	},
+});
 
-        <Shown value={open} children={children} />
-    </div>;
-};
+export default ThemeContext.use(h => {
+	const DropDown = ({
+		children,
+		label,
+		arrow = 'right',
+		style,
+		open = Observer.mutable(false),
+		iconOpen = <Icon size="20" name="chevron-down" />,
+		iconClose = <Icon size="20" name="chevron-down" />,
+		hover,
+		focused,
+		...props
+	}) => {
+		if (!(open instanceof Observer)) open = Observer.mutable(open);
+		if (!(focused instanceof Observer)) focused = Observer.mutable(false);
+		if (!(hover instanceof Observer)) hover = Observer.mutable(false);
 
-export default DropDown;
+		const toggle = () => open.set(!open.get());
+		const [ripples, createRipple] = useRipples();
+
+		if (!focused.isImmutable()) props.isFocused = focused;
+		if (!hover.isImmutable()) props.isHovered = hover;
+
+		return <div style={style}>
+			<div
+				ref
+				{...props}
+				theme={[
+					'dropdown',
+					hover.bool('hovered', null),
+					focused.bool('focused', null),
+				]}
+				onMouseEnter={() => hover.set(true)}
+				onMouseLeave={() => {
+					hover.set(false);
+					focused.set(false);
+				}}
+				onMouseDown={(event) => {
+					focused.set(true);
+					createRipple(event);
+					toggle();
+				}}
+				onKeyDown={(event) => {
+					if (event.key === 'Enter' || event.key === ' ') {
+						event.preventDefault();
+						focused.set(true);
+						createRipple(event);
+						toggle();
+					}
+				}}
+			>
+				<div theme={['row_fill_spread']}>
+					{arrow === 'right' ? <span>{label}</span> : null}
+
+					<i class="chevron-icon" style={{ display: 'flex', alignItems: 'center' }}>
+						{open.map(show => (show ? iconOpen : iconClose))}
+					</i>
+
+					{arrow === 'left' ? <span>{label}</span> : null}
+				</div>
+
+				{ripples}
+			</div>
+
+			<Shown value={open} children={children} />
+		</div>;
+	};
+
+	return DropDown;
+});
