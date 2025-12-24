dc8f2c37eb7986c05c5edeeee82973ccefd874f6
Torrin Leonard <torrin@torrin.me>
2025-12-24 17:26:35 -0500

    TextField: Updating themes, adding contained, outlined, and text variants to match Button themes.

--- components/inputs/TextField/TextField.jsx
+++ components/inputs/TextField/TextField.jsx
@@ -1,22 +1,81 @@
 import Observer from 'destam/Observer';
 
+import Theme from '../../utils/Theme/Theme.jsx';
 import ThemeContext from '../../utils/ThemeContext/ThemeContext.jsx';
+
+Theme.define({
+	field: {
+		extends: 'radius_typography_p1_regular',
+		outline: 0,
+		padding: 10,
+		border: 'none',
+		color: '$color',
+	},
+
+	field_contained: {
+		extends: 'typography_p1_bold',
+		background: '$color',
+		color: '$contrast_text($color_top)',
+	},
+
+	field_contained_hovered: {
+		background: '$color_hover',
+	},
+
+	field_contained_disabled: {
+		$bg: '$saturate($color, -1)',
+		background: '$bg',
+		color: '$contrast_text($bg)',
+	},
+
+	field_outlined: {
+		extends: 'typography_p1_bold',
+		borderWidth: 2,
+		borderStyle: 'solid',
+	},
+
+	field_outlined_hovered: {
+		background: 'rgb(0, 0, 0, 0.1)',
+	},
+
+	field_outlined_disabled: {
+		color: '$saturate($color, -1)',
+	},
+
+	field_text: {
+		extends: 'typography_p1_regular',
+	},
+
+	field_text_hovered: {
+		background: 'rgb(0, 0, 0, 0.1)',
+	},
+
+	field_text_disabled: {
+		color: '$saturate($color, -1)',
+	},
+});
 
 export default ThemeContext.use(h => {
 	const TextField = ({
 		value,
 		type = 'text',
+		style,
 		inline,
 		expand,
 		onEnter,
 		error,
-		focused = false,
+		focused,
+		disabled,
+		hover,
 		...props
 	}, cleanup, mounted) => {
 		if (!(value instanceof Observer)) value = Observer.immutable(value);
 		if (!(error instanceof Observer)) error = Observer.immutable(error);
 		if (!(expand instanceof Observer)) expand = Observer.immutable(expand);
 		if (!(focused instanceof Observer)) focused = Observer.mutable(focused);
+		if (!(hover instanceof Observer)) hover = Observer.mutable(hover);
+		if (!(disabled instanceof Observer)) disabled = Observer.mutable(disabled);
+
 
 		const ref = Observer.mutable(null);
 
@@ -25,18 +84,24 @@ export default ThemeContext.use(h => {
 			else ref.get().blur();
 		})));
 
+		if (!focused.isImmutable()) props.isFocused = focused;
+		if (!hover.isImmutable()) props.isHovered = hover;
+
 		return <input ref={ref}
 			$value={value.def('')}
 			onInput={(e) => {
+				if (disabled.get()) return;
+
 				if (value.isImmutable()) {
 					ref.get().value = value.get() || '';
 					return;
 				}
 				value.set(e.target.value);
 			}}
-			type={type}
-			isFocused={focused}
+			type='text'
 			onKeyDown={e => {
+				if (disabled.get()) return;
+
 				if (value.isImmutable()) {
 					e.preventDefault();
 				}
@@ -48,13 +113,23 @@ export default ThemeContext.use(h => {
 					}
 				}
 			}}
+			isFocused={focused}
+
+			style={{
+				display: inline ? 'inline-flex' : 'flex',
+				...style
+			}}
+			disabled={disabled}
 			{...props}
 			theme={[
 				'field',
-				'line',
+				type,
 				focused.map(e => e ? 'focused' : null),
 				error.map(e => e ? 'error' : null),
 				expand.map(e => e ? 'expand' : null),
+				hover.bool('hovered', null),
+				disabled.bool('disabled', null),
+
 			]}
 		/>;
 	};
