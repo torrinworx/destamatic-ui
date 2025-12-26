0041f480fc724ad6d0803eae2b078e3e062eb149
Torrin Leonard <torrin@torrin.me>
2025-12-26 14:37:21 -0500

    Updating and simplifying example site.

--- examples/index.jsx
+++ examples/index.jsx
@@ -1,157 +1,31 @@
-import { Observer, mount, OObject } from 'destam-dom';
-import {
-    Theme,
-    ThemeContext,
-    Typography,
-    Button,
-    Icon,
-    LoadingDots,
-    DropDown,
-    TextField,
-    TextArea,
-    Slider,
-    Tabs,
-    Paper,
-    ColorPicker,
-    Date as DateComponent,
-    PopupContext
-} from 'destamatic-ui';
-import color from 'destamatic-ui/util/color';
+import { mount, DropDown, Typography, Icon, Icons } from 'destamatic-ui';
 
-const DemoPage = () => {
-    const handleClick = () => {
-        console.log('Button clicked');
-    };
+import IconifyIcons from "destamatic-ui/components/icons/IconifyIcons/IconifyIcons";
 
-    const text = Observer.mutable('');
+const ExampleWrapper = ({ example }) => {
+    const { header, example: ExampleComp } = example;
 
-    const date = Observer.mutable(new Date());
-
-    const specialTheme = OObject({
-        special: OObject({
-            $color: 'green'
-        })
-    });
-
-    return <div $style={{
-        fontFamily: 'Roboto, sans-serif',
-        padding: '20px',
-        inset: '0px',
-        position: 'absolute',
-    }}>
-        <Tabs style={{ width: '100%' }}>
-            <mark:tab name="one">
-                One
-            </mark:tab>
-            <mark:tab name="two">
-                Two
-            </mark:tab>
-            <mark:tab name="Three">
-                Three
-            </mark:tab>
-        </Tabs>
-
-        <div theme="center" style={{ flexDirection: 'column' }}>
-            <ColorPicker value={specialTheme.observer.path(['special', '$color'])
-                .setter((val, set) => set(color.toCSS(val)))} />
-
-            <Theme value={specialTheme}>
-                <ThemeContext value="special">
-                    <DateComponent value={date} />
-                </ThemeContext>
-            </Theme>
-            <Button onClick={() => {
-                let d = new Date(date.get());
-                d.setFullYear(d.getFullYear() + 1);
-                date.set(d);
-            }}>Advance one Year</Button>
-            <div>
-                {date.map(d => d.toString())}
-            </div>
-        </div>
-
-        <Typography type="h1" label="Destamatic UI Demo" />
-        <Typography type="h2" label="Typography" />
-        <Typography type="h3" label="Heading 3 Regular" />
-        <Typography type="h3_bold" label="Heading 3 Bold" />
-        <Typography type="p1" label="Paragraph 1 Regular" />
-        <Typography type="p1_bold" label="Paragraph 1 Bold" />
-        <Typography type="p2" label="Paragraph 2 Regular" />
-        <Typography type="p2_bold" label="Paragraph 2 Bold" />
-
-        <Typography type="h2" $style={{ marginTop: '20px' }} label="Buttons" />
-        <div $style={{ display: 'flex', gap: '20px', flexWrap: 'wrap' }}>
-            <Button label="Text Button" type="text" onClick={handleClick} />
-            <Button label="Contained Button" type="contained" onClick={handleClick} />
-            <Button label="Outlined Button" type="outlined" onClick={handleClick} />
-            <Button
-                label="Icon Outline"
-                type="outlined"
-                onClick={handleClick}
-                icon={<Icon name="feather" />}
-            />
-            <Button
-                label="Icon Contained"
-                type="contained"
-                onClick={handleClick}
-                icon={<Icon name="feather" />}
-            />
-        </div>
-        <Theme value={{
-            primary: {
-                $color: 'pink',
-                $color_hover: 'blue',
-                $color_top: 'white',
-            },
-
-            myContent: {
-                color: 'red',
-            },
-
-            button_contained: {
-                background: 'linear-gradient($color, $hue($color, 0.1))'
-            }
-        }}>
-            <div $style={{ display: 'flex', gap: '20px', flexWrap: 'wrap' }}>
-                <Button label="Text Button" type="text" onClick={handleClick} />
-                <Button label="Contained Button" type="contained" onClick={handleClick} />
-                <Button label="Outlined Button" type="outlined" onClick={handleClick} />
-                <Button
-                    label="Icon Outline"
-                    type="outlined"
-                    onClick={handleClick}
-                    icon={<Icon name="feather" />}
-                />
-                <Button
-                    label="Icon Contained"
-                    type="contained"
-                    onClick={handleClick}
-                    icon={<Icon name="feather" />}
-                />
-            </div>
-
-            <Typography type="h2" $style={{ marginTop: '20px' }} label="Dropdown" />
-            <DropDown label="Click to Toggle">
-                <div theme="myContent">
-                    Dropdown Content
-                </div>
-            </DropDown>
-        </Theme>
-
-        <Typography type="h2" $style={{ marginTop: '20px' }} label="Loading Dots" />
-        <LoadingDots />
-
-        <Typography type="h2" $style={{ marginTop: '20px' }} label="Slider" />
-        <Slider min={0} max={100} value={Observer.mutable(0)} />
-
-        <Paper>
-            <Typography type="h2" $style={{ marginTop: '20px' }} label="Inputs" />
-            <TextField placeholder="Type here..." style={{ marginBottom: '10px', width: '100%' }} value={text} />
-            <TextArea placeholder="Enter more text here..." style={{ width: '100%' }} value={text} />
-        </Paper>
-    </div>;
+    return <DropDown
+        open={example.open}
+        label={<Typography type="p1" label={header} />}
+        iconOpen={<Icon name="feather:chevron-up" />}
+        iconClose={<Icon name="feather:chevron-down" />}
+    >
+        <ExampleComp />
+    </DropDown>;
 };
 
-mount(document.body, <PopupContext>
-    <DemoPage />
-</PopupContext>);
+const Examples = () => {
+    const example_array = Object.values(
+        import.meta.glob(
+            '../../destamatic-ui/components/**/**/*.example.jsx',
+            { eager: true }
+        )
+    ).map(e => e.default);
+
+    return <Icons value={[IconifyIcons]}>
+        <ExampleWrapper each:example={example_array} />
+    </Icons>;
+};
+
+mount(document.body, <Examples />);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import fs from 'fs';
-import path from 'path';
-
 import { defineConfig } from 'vite';
+
 import assertRemove from 'destam-dom/transform/assertRemove';
 import compileHTMLLiteral from 'destam-dom/transform/htmlLiteral';
 
@@ -16,7 +14,7 @@ const createTransform = (name, transform, jsx, options) => ({
 			});
 			return {
 				code: transformed.code,
-				map: transformed.decodedMap,
+				map: transformed.map,
 			};
 		}
 	}
@@ -26,107 +24,54 @@ const plugins = [];
 
 plugins.push(createTransform('transform-literal-html', compileHTMLLiteral, true, {
 	jsx_auto_import: {
-		'h': 'destamatic-ui',
+		h: 'destamatic-ui',
 		'mark': 'destamatic-ui',
-		'raw': { name: 'h', location: 'destam-dom' },
+		raw: {
+			name: 'h',
+			location: 'destam-dom'
+		}
 	},
 }));
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.ENV === 'production') {
 	plugins.push(createTransform('assert-remove', assertRemove));
 }
 
-const recursiveReadDir = (dir, fileList = []) => {
-	fs.readdirSync(dir, { withFileTypes: true }).forEach(entry => {
-		const fullPath = path.resolve(dir, entry.name);
-		if (entry.isDirectory()) {
-			recursiveReadDir(fullPath, fileList);
-		} else if (entry.name.endsWith('.js') || entry.name.endsWith('.jsx')) {
-			fileList.push(fullPath);
-		}
-	});
-	return fileList;
-};
-
-const getExample = name => {
-	name = name.endsWith('.html') ? name.slice(0, -5) : name + 'index';
-	name = name.startsWith('/') ? name.slice(1) : name;
-	const variants = ['.js', '.jsx', '.example.js', '.example.jsx'];
-	const existed = variants.find(v => fs.existsSync(path.resolve(__dirname, 'examples', name + v)));
-	if (!existed) return null;
-	const relative = '/' + name + '.html';
-	return {
-		name,
-		file: name + existed,
-		relative,
-		location: path.resolve(__dirname, 'examples', name + existed),
-		resolved: path.join(__dirname, relative),
-	};
-};
-
-let examples;
-const getExamples = () => {
-	if (examples) return examples;
-	const examplesDir = path.resolve(__dirname, 'examples');
-	return examples = recursiveReadDir(examplesDir).map(fullPath => {
-		const relativePath = fullPath.substring(fullPath.indexOf('examples') + 'examples/'.length);
-		const name = relativePath.substring(0, relativePath.lastIndexOf('.'));
-		return getExample(`/${name}.html`);
-	}).filter(Boolean);
-};
-
-const generateTemplate = (entry, hot) => {
-	return `
-		<!doctype html>
-		<html lang="en">
-			${hot ? '<script type="module" src="/@vite/client"></script>' : ""}
-			<head>
-				<meta charset="UTF-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<title>Destamatic UI - ${entry.name}</title>
-				<link rel="icon" href="data:;base64,iVBORw0KGgo=">
-			</head>
-			<body>
-				<div id="app"></div>
-				<script type="module" src="/examples/${entry.file}"></script>
-			</body>
-		</html>
-	`.trim();
-};
-
-plugins.push({
-	name: 'examples',
-	resolveId(id) {
-		let found = getExamples().find(ex => ex.resolved === id);
-		if (found) {
-			return found.resolved;
-		}
-	},
-	load(id) {
-		let found = getExamples().find(ex => ex.resolved === id);
-		if (found) return generateTemplate(found);
-	},
-	configureServer(server) {
-		server.middlewares.use((req, res, next) => {
-			let found = getExample(req.originalUrl);
-			if (found) {
-				res.end(generateTemplate(found, true));
-			} else {
-				next();
-			}
-		});
-	},
-});
-
 export default defineConfig({
+	root: './examples',
 	plugins,
 	esbuild: {
 		jsx: 'preserve',
 	},
+	base: '',
 	resolve: {
-		alias: [
-			{ find: /^destamatic-ui($|\/)/, replacement: '/' },
-			{ find: '@public', replacement: '/examples' },
-		],
-	}
+		extensions: ['.js', '.ts', '.tsx', '.jsx'],
+		alias: {
+			util: 'util/',
+		},
+	},
+	define: {
+		'process.env': {},
+		'process.env.NODE_ENV': '"development"',
+		global: 'globalThis',
+	},
+	server: {
+		host: true,
+		port: process.env.PORT || 3000,
+	},
+	optimizeDeps: {
+		include: ['@babel/parser', '@babel/generator', '@babel/types', 'util'],
+		exclude: ['destamatic-ui', 'destam-dom', 'destam'],
+	},
+	build: {
+		target: 'esnext',
+		outDir: '../build/dist',
+		rollupOptions: {
+			output: {
+				entryFileNames: '[name].js',
+				chunkFileNames: 'assets/[name].[hash].js',
+				assetFileNames: 'assets/[name].[hash][extname]',
+			},
+		},
+	},
 });
