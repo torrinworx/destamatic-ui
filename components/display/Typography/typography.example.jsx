17b68a1bf5f59cc4da3e4ea309ffc0a5e2e2a258
Torrin Leonard <torrin@torrin.me>
2025-12-26 14:49:35 -0500

    Typography: Addin better example.

--- components/display/Typography/typography.example.jsx
+++ components/display/Typography/typography.example.jsx
@@ -1,9 +1,139 @@
-import { mount } from 'destam-dom';
-import { Typography } from 'destamatic-ui';
+import { Typography, TextModifiers, TextField, Radio, Toggle, Observer } from 'destamatic-ui';
 
 const Example = () => {
+	const type = Observer.mutable('h2')
+	const SelectRadio = Radio(type);
+	const italic = Observer.mutable(false);
+	const bold = Observer.mutable(false);
 
-	return <Typography type='h1' label='hello world!' />
-}
+	const value = Observer.mutable('Try typing: TODO, @mention, #tag, *emphasis*, or !!strong!! text.');
+	const modifiers = [
+		{
+			check: /\b(TODO|DONE)\b/g,
+			atomic: true,
+			return: match => (
+				<span
+					style={{
+						display: 'inline-block',
+						padding: '0 4px',
+						borderRadius: 4,
+						background: match === 'DONE' ? 'green' : 'orange',
+						color: 'white',
+						fontWeight: 600,
+					}}
+				>
+					{match}
+				</span>
+			),
+		},
+		{
+			check: /@\w+/g,
+			atomic: false,
+			return: match => (
+				<span
+					style={{
+						display: 'inline-block',
+						color: 'blue',
+						fontWeight: 500,
+					}}
+				>
+					{match}
+				</span>
+			),
+		},
+		{
+			check: /#\w+/g,
+			atomic: false,
+			return: match => (
+				<span
+					style={{
+						display: 'inline-block',
+						color: 'green',
+						fontWeight: 500,
+					}}
+				>
+					{match}
+				</span>
+			),
+		},
+		{
+			check: /!!(.+?)!!/g,
+			atomic: false,
+			return: match => (
+				<span style={{ fontWeight: 700, display: 'inline-block' }}>
+					{match.slice(2, -2)}
+				</span>
+			),
+		},
+		{
+			check: /\*(.+?)\*/g,
+			atomic: false,
+			return: match => (
+				<span style={{ fontStyle: 'italic', display: 'inline-block' }}>
+					{match.slice(1, -1)}
+				</span>
+			),
+		},
+	];
 
-mount(root, <Example />);
+	return <>
+		<Typography />
+		<div theme='row_fill_center_wrap' >
+			<SelectRadio value={'h1'} label='h1' />
+			<SelectRadio value={'h2'} label='h2' />
+			<SelectRadio value={'h3'} label='h3' />
+			<SelectRadio value={'h4'} label='h4' />
+			<SelectRadio value={'h5'} label='h5' />
+			<SelectRadio value={'h6'} label='h6' />
+			<SelectRadio value={'p1'} label='p1' />
+			<SelectRadio value={'p2'} label='p2' />
+		</div>
+		<div theme='row_fill_center_wrap'>
+			<div theme='row'>
+				<Typography type='p1' label='Bold: ' />
+				<Toggle value={bold} />
+			</div>
+			<div theme='row'>
+				<Typography type='p1' label='Italic: ' />
+				<Toggle value={italic} />
+			</div>
+		</div>
+
+		<div theme='divider' />
+		<div theme='column_center'>
+			<Typography
+				type={Observer.all([type, bold, italic]).map(
+					([t, b, i]) =>
+						[t, b && 'bold', i && 'italic'].filter(Boolean).join('_')
+				)}
+				label={`The quick brown fox jumps over the lazy dog.`}
+			/>
+		</div>
+
+		<div theme='divider' />
+
+		<div theme='row_fill_center_wrap'>
+			<Typography type='p1' label='Text Modifiers:' />
+			<TextField expand type='outlined' value={value} />
+		</div>
+		<div theme='divider' />
+
+		<div theme='column_center'>
+			<TextModifiers value={modifiers} >
+				<Typography
+					type={Observer.all([type, bold, italic]).map(
+						([t, b, i]) =>
+							[t, b && 'bold', i && 'italic'].filter(Boolean).join('_')
+					)}
+					label={value}
+				/>
+			</TextModifiers>
+		</div>
+	</>;
+};
+
+export default {
+	open: true,
+	example: Example,
+	header: 'Typography',
+};
