71d0fbb4d435ba33610ceeca003884fcd8468f34
Torrin Leonard <torrin@torrin.me>
2025-12-27 16:16:57 -0500

    button.example: removing old button example

--- examples/inputs/button.example.jsx
+++
@@ -1,9 +0,0 @@
-import { mount } from 'destam-dom';
-import { Button, Typography } from 'destamatic-ui';
-
-mount(document.body, <>
-	<Button label="hello" type='contained' onClick={() => { }} />
-	<Button label="world" type='outlined' onClick={() => { }} />
-	<Button label={<Typography type='h1' label='hello world' />} type='outlined' onClick={() => { }} />
-
-</>);
