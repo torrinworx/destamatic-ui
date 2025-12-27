e733a4c0e4400b4fa63dbcf3c6e40b938fe346ba
Torrin Leonard <torrin@torrin.me>
2025-12-27 16:56:04 -0500

    Updating vite test to new directory structure

--- vitest.config.js
+++ vitest.config.js
@@ -5,16 +5,11 @@ import baseViteConfig from './vite.config.js'
 export default mergeConfig(
   baseViteConfig,
   defineConfig({
+    root: '.',
     test: {
       environment: 'jsdom',
-      setupFiles: ['./tests/document.js'],
-      include: ['tests/**/*.test.{js,ts,jsx,tsx}', 'tests/**/*.spec.{js,ts,jsx,tsx}'],
+      setupFiles: ['document.js'],
+      include: ['components/**/*.test.{js,ts,jsx,tsx}', 'components/**/*.spec.{js,ts,jsx,tsx}'],
     },
-  })
+  }),
 );
-
-// todo: dual test system, one that tests form the source, and another that tests
-// from the dist resulting from vite build. we should map destamatic-ui imports in the
-// tests to the ./dist/index.js, so that might mean changing/loading two different vite
-// configs or doing something withhere iwth vitest? idk. would be nice to verify on 
-// npm push that the build works.
