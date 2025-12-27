29356d9a0f38d029a40eabf93e0a890d36d42235
Torrin Leonard <torrin@torrin.me>
2025-12-27 13:05:41 -0500

    button.example: updateing button example file

--- components/inputs/Button/button.example.jsx
+++ components/inputs/Button/button.example.jsx
@@ -1,15 +1,86 @@
-import { mount } from 'destam-dom';
-import { Button } from 'destamatic-ui';
+import { Button, Typography, Toggle, Observer, Icon, Radio } from 'destamatic-ui';
 
 const Example = () => {
-	const onClick = () => console.log("Hello World!");
+	const type = Observer.mutable('contained')
+	const SelectRadio = Radio(type);
+	const disable_buttons = Observer.mutable(false);
+	const doneCheck = Observer.mutable(false);
+	doneCheck.watch(() => {
+		if (doneCheck.get()) {
+			setTimeout(() => {
+				doneCheck.set(false);
+			}, 1200);
+		}
+	});
 
-	return <div theme='column_center' style={{ padding: 10, gap: 10 }}>
-		<Button type='contained' label='Contained' onClick={onClick} />
-		<Button type='outlined' label='Outlined' onClick={onClick} />
-		<Button type='text' label='Text' onClick={onClick} />
-		<Button type='contained' label='Disabled' disabled onClick={onClick} />
+	return <div style={{ padding: 10 }}>
+		<div theme='row_fill_center_wrap' >
+			<SelectRadio value={'contained'} label='contained' />
+			<SelectRadio value={'outlined'} label='outlined' />
+			<SelectRadio value={'text'} label='text' />
+			<SelectRadio value={'link'} label='link' />
+		</div>
+		<div theme='row_fill_center_warp'>
+			<Typography type='p1' label='Disable: ' />
+			<Toggle value={disable_buttons} />
+		</div>
+
+		<div theme='divider' />
+
+		<div theme='row_center_wrap'>
+			<Button
+				type={type}
+				label="Click Me"
+				disabled={disable_buttons}
+				onClick={() => { }}
+			/>
+			<Button
+				type={type}
+				disabled={disable_buttons}
+				iconPosition="right"
+				label="Click Me"
+				icon={<Icon name="feather:external-link" />}
+				onClick={() => { }}
+			/>
+			<Button
+				type={type}
+				disabled={disable_buttons}
+				iconPosition="left"
+				label="Click Me"
+				icon={<Icon name="feather:external-link" />}
+				onClick={() => { }}
+			/>
+			<Button
+				round
+				type={type}
+				disabled={disable_buttons}
+				icon={<Icon name="feather:external-link" />}
+				onClick={() => { }}
+			/>
+			<Button
+				type={type}
+				disabled={disable_buttons}
+				icon={<Icon name="feather:external-link" />}
+				onClick={() => { }}
+			/>
+			<Button
+				type={type}
+				iconPosition="right"
+				disabled={disable_buttons}
+				label={doneCheck.map(c => (c ? "Done!" : "Delay"))}
+				icon={doneCheck.map(c =>
+					c ? <Icon name="feather:check" />
+						: <Icon name="feather:upload" />
+				)}
+				onClick={async () => {
+					return new Promise(ok => setTimeout(() => { doneCheck.set(true); ok(); disable_buttons.set(false) }, 1000))
+				}}
+			/>
+		</div>
 	</div>;
 };
 
-mount(root, <Example />);
+export default {
+	example: Example,
+	header: 'Button',
+};
