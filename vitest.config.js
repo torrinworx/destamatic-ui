b8e6e8b556d40997be096fba18462ac4e208ba6d
Torrin Leonard <torrin@torrin.me>
2025-12-27 17:04:33 -0500

    Changing location of vitetest document

--- vitest.config.js
+++ vitest.config.js
@@ -8,7 +8,7 @@ export default mergeConfig(
     root: '.',
     test: {
       environment: 'jsdom',
-      setupFiles: ['document.js'],
+      setupFiles: ['util/document.js'],
       include: ['components/**/*.test.{js,ts,jsx,tsx}', 'components/**/*.spec.{js,ts,jsx,tsx}'],
     },
   }),
