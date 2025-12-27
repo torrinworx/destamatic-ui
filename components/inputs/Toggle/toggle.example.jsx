11b7bb5bba6f5961a5446027f4ec94a91e632836
Torrin Leonard <torrin@torrin.me>
2025-12-27 18:03:48 -0500

    toggle.example:Creating new toggle example

---
+++ components/inputs/Toggle/toggle.example.jsx
@@ -0,0 +1,29 @@
+import { Toggle, Typography, Observer, Radio } from 'destamatic-ui';
+
+const Example = () => {
+	const type = Observer.mutable('outlined')
+	const SelectRadio = Radio(type);
+	const disable_toggles = Observer.mutable(false);
+
+	return <div style={{ padding: 10 }}>
+		<div theme='row_fill_center_wrap' >
+			<SelectRadio value={'outlined'} label='outlined' />
+			<SelectRadio value={'contained'} label='contained' />
+		</div>
+		<div theme='row_fill_center_wrap'>
+			<Typography type='p1' label='Disable: ' />
+			<Toggle value={disable_toggles} />
+		</div>
+
+
+		<div theme='divider' />
+		<div theme='row_center_wrap'>
+			<Toggle type={type} value={Observer.mutable(false)} disabled={disable_toggles} />
+		</div>
+	</div>;
+};
+
+export default {
+	example: Example,
+	header: 'Toggle',
+};
