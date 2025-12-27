f90531daafec390ef8baa86b65bf47bd4b022273
Torrin Leonard <torrin@torrin.me>
2025-12-27 16:17:37 -0500

    Slider: Removing white spaces

--- components/inputs/Slider/Slider.jsx
+++ components/inputs/Slider/Slider.jsx
@@ -27,12 +27,10 @@ Theme.define({
 		position: 'relative',
 		width: '100%',
 		height: '100%',
-
 		display: 'flex',
 		alignItems: 'center',
 		justifyContent: 'center',
 		userSelect: 'none',
-
 	},
 
 	slidertrack: {
