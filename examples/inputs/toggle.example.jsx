72ae37cf2c0f65b286789391546ffca8b2132106
Torrin Leonard <torrin@torrin.me>
2025-12-27 18:19:58 -0500

    Removing old toggle example

--- examples/inputs/toggle.example.jsx
+++
@@ -1,8 +0,0 @@
-import { mount, Observer } from 'destam-dom';
-import { Toggle } from 'destamatic-ui';
-
-const value = Observer.mutable(false);
-
-mount(document.body, <div style={{ display: 'flex', flexDirection: 'column', gap: 20, margin: 10 }}>
-    <Toggle value={value} />
-</div>);
