583f466e5a016d7b778052bc852275b9cea1de50
Alexander Orzechowski <alex@ozal.ski>
2026-01-20 13:43:11 -0500

    select.example.jsx: Move PopupContext to root
    PopupContext is where all the popups are mounted, the context should
    exist on the top of the page

--- components/inputs/Select/select.example.jsx
+++ components/inputs/Select/select.example.jsx
@@ -1,4 +1,4 @@
-import { Typography, Observer, PopupContext, Popup, Select } from 'destamatic-ui';
+import { Typography, Observer, Select } from 'destamatic-ui';
 
 const Example = () => {
 	const input = Observer.mutable('');
@@ -20,67 +20,62 @@ const Example = () => {
 				overflow: 'hidden',
 			}}
 		>
-			<PopupContext>
-				  <Popup />
+			<div
+				style={{
+					position: 'absolute',
+					inset: 0,
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+					flexDirection: 'column',
+					gap: 10,
+				}}
+			>
+				<div style={{ pointerEvents: 'auto' }}>
+					<Typography label={input} />
+					<Select
+						options={['one', 'two', 'three']}
+						style={{ width: 100 }}
+						type="contained"
+						value={Observer.mutable('one')}
+					/>
+					<Select
+						options={Array(70).fill(null).map((_, i) => String.fromCharCode(i + 41))}
+						style={{ width: 100 }}
+						type="contained"
+						value={Observer.mutable('a')}
+					/>
+				</div>
+			</div>
 
-				<div
+			{[
+				['left', 'top'],
+				['right', 'top'],
+				['left', 'bottom'],
+				['right', 'bottom'],
+			].map(([a, b]) => (
+				<Select
+					key={`${a}-${b}`}
 					style={{
 						position: 'absolute',
-						inset: 0,
-						display: 'flex',
-						alignItems: 'center',
-						justifyContent: 'center',
-						flexDirection: 'column',
-						gap: 10,
+						[a]: 8,
+						[b]: 8,
+						width: 120,
 					}}
-				>
-					<div style={{ pointerEvents: 'auto' }}>
-						<Typography label={input} />
-						<Select
-							options={['one', 'two', 'three']}
-							style={{ width: 100 }}
-							type="contained"
-							value={Observer.mutable('one')}
-						/>
-						<Select
-							options={Array(70).fill(null).map((_, i) => String.fromCharCode(i + 41))}
-							style={{ width: 100 }}
-							type="contained"
-							value={Observer.mutable('a')}
-						/>
-					</div>
-				</div>
+					value={input}
+					options={Array(100).fill(null).map((_, i) => i)}
+				/>
+			))}
 
-				{[
-					['left', 'top'],
-					['right', 'top'],
-					['left', 'bottom'],
-					['right', 'bottom'],
-				].map(([a, b]) => (
-					<Select
-						key={`${a}-${b}`}
-						style={{
-							position: 'absolute',
-							[a]: 8,
-							[b]: 8,
-							width: 120,
-						}}
-						value={input}
-						options={Array(100).fill(null).map((_, i) => i)}
-					/>
-				))}
-
-				<div style={{ position: 'absolute', left: 8, top: '50%', transform: 'translateY(-50%)' }}>
-					<Select
-						placeholder="Functions"
-						options={Object.keys(options)}
-						value={Observer.immutable().setter((key) => options[key]())}
-						style={{ width: 150, border: 'none' }}
-						type="text"
-					/>
-				</div>
-
-			</PopupContext>
+			<div style={{ position: 'absolute', left: 8, top: '50%', transform: 'translateY(-50%)' }}>
+				<Select
+					placeholder="Functions"
+					options={Object.keys(options)}
+					value={Observer.immutable().setter((key) => options[key]())}
+					style={{ width: 150, border: 'none' }}
+					type="text"
+				/>
+			</div>
 		</div>
 	</div>;
 };
